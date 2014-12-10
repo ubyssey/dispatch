5f01f937e093dd96646ea9db52be7a89dc8127c1
Peter Siemens <peterjsiemens@gmail.com>
2014-12-10 15:25:18 -0800

    moved api routes out of root

--- dispatch/dispatch/urls.py
+++ dispatch/dispatch/urls.py
@@ -11,6 +11,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
