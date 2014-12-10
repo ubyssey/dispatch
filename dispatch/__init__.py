b85f03ebe69df81f507e7a6431a77d69718da8ce
captainkrtek <steven.richardspc@gmail.com>
2014-12-09 23:32:27 -0800

    modified author

--- dispatch/__init__.py
+++ dispatch/__init__.py
@@ -1,1 +1,1 @@
-__author__ = 'krtek'
+__author__ = 'Steven Richards'
--- dispatch/api/views.py
+++ dispatch/api/views.py
@@ -1,3 +1,4 @@
+__author__ = 'Steven Richards'
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
