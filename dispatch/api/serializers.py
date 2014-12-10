c98d4136400914aae181b17c21698dff7dcbb73c
captainkrtek <steven.richardspc@gmail.com>
2014-12-09 23:25:32 -0800

    updated imports

---
+++ dispatch/api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+#class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+#   class Meta:
+#        model = Article
+#        fields = ('headline')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        User = get_user_model()
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
