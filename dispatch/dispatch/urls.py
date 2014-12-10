108ff3c1098bf084fea19696b984cb3ce4efadea
captainkrtek <steven.richardspc@gmail.com>
2014-12-09 22:36:57 -0800

    initial api testing for REST framework

--- dispatch/dispatch/urls.py
+++ dispatch/dispatch/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dispatch.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
