6d7c33be3362bdd9aa622b8f208e393843810a3d
Peter Siemens <peterjsiemens@gmail.com>
2014-12-10 14:10:45 -0800

    took out unneeded admin class

--- dispatch/core/admin.py
+++ dispatch/core/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import User, Person, Contributor, ContributorType
 
-class UserAdmin(admin.ModelAdmin):
-    pass
-
 admin.site.register(User)
 admin.site.register(Person)
 admin.site.register(Contributor)
