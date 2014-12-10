e4bde810f2bc8c1af1c213c8744f78dac15605e4
captainkrtek <steven.richardspc@gmail.com>
2014-12-09 23:32:27 -0800

    modified author

--- dispatch/api/serializers.py
+++ dispatch/api/serializers.py
@@ -1,3 +1,4 @@
+__author__ = 'Steven Richards'
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
@@ -17,4 +18,3 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
-
