66ca1e3aff780a53ff987cb6569e1b450f31ec94
Ben Cook <b@bencook.ca>
2014-12-10 02:58:04 -0800

    Add test for Section uniqueness; rm unnecessary tests

--- dispatch/content/tests.py
+++ dispatch/content/tests.py
@@ -1,32 +1,14 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.db import IntegrityError
 from content.models import Section, Article, Image
 
 class SectionTests(TestCase):
     NAME = "News"
 
     def setUp(self):
-        self.section = Section.objects.create(name=self.NAME)
+        self.s1 = Section.objects.create(name=self.NAME)
 
-    def test_add_section(self):
-        self.assertEqual(self.section.name,
-                         self.NAME)
-
-
-class ArticleTests(TestCase):
-    def setUp(self):
-        section = Section.objects.create(name="Sports")
-
-        self.A1 = {
-            "long_headline": "Buchanan Tower Beats One Yonge Street as Ugliest Building",
-            "section": section,
-            "published_at": timezone.now(),
-            "content": "Refer to headline."
-        }
-
-        self.test_a1 = Article.objects.create(**self.A1)
-
-    def test_add_article(self):
-        for key, expected in self.A1.viewitems():
-            self.assertEqual(getattr(self.test_a1, key),
-                             expected)
+    def test_no_duplicate_section_names(self):
+        with self.assertRaises(IntegrityError):
+            s2 = Section.objects.create(name=self.NAME)
