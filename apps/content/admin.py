1d50ee6fa79ff796bdb2b3f4ece4d1fc5b07bd2e
Peter Siemens <peterjsiemens@gmail.com>
2014-12-11 22:47:20 -0800

    playing around with custom admin templates

--- apps/content/admin.py
+++ apps/content/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.tests.content.models import Article, Section, Image
+from apps.content.models import Article, Section, Image
 
 class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("short_headline",)}
