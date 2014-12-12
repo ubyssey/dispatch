bee5595628718d31d27c488e613d0ac39779d1d5
captainkrtek <steven.richardspc@gmail.com>
2014-12-11 16:41:31 -0800

    initial addition of articles, resources

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,12 +1,24 @@
 __author__ = 'Steven Richards'
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
+from dispatch.apps.content.models import Resource, Article
 from rest_framework import serializers
 
-#class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-#   class Meta:
-#        model = Article
-#        fields = ('headline')
+class ResourceSerializer(serializers.HyperlinkedModelSerializer):
+   class Meta:
+        model = Resource
+        fields = ('created_at','updated_at')
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('long_headline',
+                  'short_headline',
+                  'section',
+                  'is_published',
+                  'published_at',
+                  'slug',
+                  'content')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
