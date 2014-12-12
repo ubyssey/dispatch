e87460442a97ab3f401c8c912eb22df10a6a6268
captainkrtek <steven.richardspc@gmail.com>
2014-12-11 16:41:44 -0800

    add routing

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
-from api import views
+from dispatch.apps.api import views
 
 admin.autodiscover()
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'resources', views.ResourceViewSet)
+router.register(r'articles', views.ArticleViewSet)
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
