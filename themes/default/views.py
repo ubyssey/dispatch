3d048ba3978132e6de9c1e8fa2fa39fb6476c563
Peter Siemens <peterjsiemens@gmail.com>
2014-12-12 21:26:02 -0800

    added custom template loader

--- themes/default/views.py
+++ themes/default/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 from apps.content.models import Article
+
+def home(request):
+    return render_to_response('index.html')
 
 def article(request, section=False, slug=False):
     if slug and section:
         article = Article.objects.get(slug=slug, section__name=section)
-    html = "<html><body>%s</body></html>" % article.long_headline
-    return HttpResponse(html)
+    context = {
+        'article': article
+    }
+    return render_to_response('article.html', context)
