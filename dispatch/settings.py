396a8ba67276f89f5aab0437d21cfe08fc8a1ad7
captainkrtek <steven.richardspc@gmail.com>
2014-12-13 16:50:05 -0800

    settings

--- dispatch/settings.py
+++ dispatch/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = 'dispatch.wsgi.application'
 if 'test' in sys.argv:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
-            'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test.db'),
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.path.join(os.path.dirname(__file__), 'test_dispatch.db'),
+            'TEST_NAME': os.path.join(os.path.dirname(__file__), 'test_dispatch.db'),
        }
     }
 
