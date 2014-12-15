da937211f6d1ecfbc9d60690d87cfa3400e58660
captainkrtek <steven.richardspc@gmail.com>
2014-12-14 16:32:36 -0800

    author not working

--- apps/content/models.py
+++ apps/content/models.py
@@ -2,7 +2,7 @@ from django.db.models import (
     Model, DateTimeField, CharField, TextField,
     ImageField, BooleanField, ForeignKey, SlugField)
 
-from dispatch.apps.core.models import Person
+from apps.core.models import Person
 
 class Resource(Model):
     created_at = DateTimeField(auto_now_add=True)
@@ -17,7 +17,7 @@ class Article(Resource):
     long_headline = CharField(max_length=200)
     short_headline = CharField(max_length=100)
     section = ForeignKey('Section')
-    author = ForeignKey(Person)
+    #author = ForeignKey(Person)
     is_published = BooleanField(default=False)
     published_at = DateTimeField()
     slug = SlugField(unique=True)
