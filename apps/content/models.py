e8631374c1a2153ab36de130a1a179cc3ff60182
Peter Siemens <peterjsiemens@gmail.com>
2014-12-14 23:26:18 -0800

    author

--- apps/content/models.py
+++ apps/content/models.py
@@ -17,7 +17,7 @@ class Article(Resource):
     long_headline = CharField(max_length=200)
     short_headline = CharField(max_length=100)
     section = ForeignKey('Section')
-    aauthor = ForeignKey(Person)
+    author = ForeignKey(Person)
     is_published = BooleanField(default=False)
     published_at = DateTimeField()
     slug = SlugField(unique=True)
