85c32e9e95538c30a14b5124cf61188fc83791b4
Peter Siemens <peterjsiemens@gmail.com>
2015-01-12 21:21:57 -0800

    attempting to fix import error

--- dispatch/apps/manager/urls.py
+++ dispatch/apps/manager/urls.py
@@ -5,5 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include(admin.site.urls)),
-    url(r'articles/?', 'dispatch.apps.admin.views.articles')
+    url(r'articles/?', 'dispatch.apps.manager.views.articles')
 )
