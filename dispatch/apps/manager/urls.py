2a385caab700d7ce1b429d299991089dc3ccc4d3
Peter Siemens <peterjsiemens@gmail.com>
2015-01-26 23:27:59 -0800

    updating url patterns for article admin

--- dispatch/apps/manager/urls.py
+++ dispatch/apps/manager/urls.py
@@ -7,5 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^', include(admin.site.urls)),
     url(r'articles/?', views.articles),
-    url(r'article/(\d*)/?$', views.article_edit)
+    url(r'article/(\d*)/?$', views.article_edit),
+    url(r'article', views.article_add),
 )
