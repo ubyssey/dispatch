83eb5dbdfe8399c0d88d5ab8fbd239a68da0cf68
Peter Siemens <peterjsiemens@gmail.com>
2015-01-26 23:28:19 -0800

    updating article manager views

--- dispatch/apps/manager/views.py
+++ dispatch/apps/manager/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
+from dispatch.apps.core.models import User
 from .forms import ArticleForm
 
 @staff_member_required
@@ -15,6 +16,39 @@ def articles(request):
 
 
 @login_required
+def article_add(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #tags = request.POST.get("tags-list", False)
+            #if tags:
+            #    a.add_tags(tags)
+        else:
+            print form.errors
+
+    else:
+        form = ArticleForm()
+
+    #tags = ",".join(a.tags.values_list('name', flat=True))
+
+    #images = a.images.all()
+    #image_ids = ",".join([str(i) for i in a.images.values_list('id', flat=True)])
+
+    user = User.objects.get(email=request.user)
+
+    a = Article()
+    a.authors.add("")
+
+    context = {
+        'article': a,
+        'form': form,
+        'tags': tags,
+    }
+
+    return render(request, 'admin/article/edit.html', context)
+
+@login_required
 def article_edit(request, id):
     a = Article.objects.get(pk=id)
     if request.method == 'POST':
@@ -22,8 +56,11 @@ def article_edit(request, id):
         if form.is_valid():
             form.save()
             tags = request.POST.get("tags-list", False)
+            attachments = request.POST.get("attachment-list", False)
             if tags:
                 a.add_tags(tags)
+            if attachments:
+                a.add_attachments(attachments)
         else:
             print form.errors
 
