087767baaaaca27efa71e6e7ed323e9177a84f47
Peter Siemens <peterjsiemens@gmail.com>
2015-02-27 19:14:01 -0800

    moved Article add/edit functionality into one form

--- dispatch/apps/manager/forms.py
+++ dispatch/apps/manager/forms.py
@@ -9,16 +9,31 @@ class BaseImageAttachmentFormSet(BaseInlineFormSet):
         if self.instance:
             self.queryset = ImageAttachment.objects.filter(article=self.instance).exclude(id=self.instance.featured_image.id)
 
-
 class ImageAttachmentForm(ModelForm):
     class Meta:
         model = ImageAttachment
         fields = '__all__'
 
         widgets = {
+            'caption': TextInput(attrs={
+                'placeholder': 'Enter a caption',
+            }),
             'image': TextInput()
         }
 
+class FeaturedImageForm(ModelForm):
+    class Meta:
+        model = ImageAttachment
+        fields = '__all__'
+
+        exclude = ('article',)
+
+        widgets = {
+            'caption': TextInput(attrs={
+                'placeholder': 'Enter a caption',
+            }),
+            'image': TextInput()
+        }
 
 class ArticleForm(ModelForm):
     class Meta:
@@ -37,19 +52,57 @@ class ArticleForm(ModelForm):
                 'placeholder': 'Write a story...',
                 'class': 'content',
             }),
-            'featured_image': TextInput(),
         }
 
-class FeaturedImageForm(ModelForm):
-    class Meta:
-        model = ImageAttachment
-        fields = '__all__'
+    # Override
+    def __init__(self, *args, **kwargs):
+        super(ArticleForm, self).__init__(*args, **kwargs)
 
-        exclude = ('article',)
+        # Pass POST data into subforms if available
+        if self.data:
+            self.attachments_form = ImageAttachmentFormSet(self.data, instance=self.instance)
+            self.featured_image_form = FeaturedImageForm(self.data, instance=self.instance.featured_image)
+        else:
+            self.attachments_form = ImageAttachmentFormSet(instance=self.instance)
+            self.featured_image_form = FeaturedImageForm(instance=self.instance.featured_image)
 
-        widgets = {
-            'image': TextInput()
-        }
+    def is_valid(self):
+        """
+        Performs validation checks on self, attachments and featured image.
 
+        Returns True if all three are valid, False otherwise.
+        """
+        article_is_valid = super(ArticleForm, self).is_valid()
+        attachments_is_valid = self.attachments_form.is_valid()
+        featured_image_is_valid = self.featured_image_form.is_valid()
+
+        return article_is_valid and attachments_is_valid and featured_image_is_valid
+
+    # Override
+    def save(self):
+
+        # Save related data (tags, topics, etc)
+        self.instance.save_related(self.data)
+
+        # Handle featured image saving
+        self.save_featured_image()
+
+        # Handle image attachments saving
+        self.save_attachments()
+
+        return super(ArticleForm, self).save()
+
+
+    def save_featured_image(self):
+        self.featured_image_form = FeaturedImageForm(self.data)
+        if self.featured_image_form.has_changed() and self.featured_image_form.is_valid():
+            self.instance.featured_image = self.featured_image_form.save()
+
+    def save_attachments(self):
+        if self.attachments_form.has_changed() and self.attachments_form.is_valid():
+            attachments = self.attachments_form.save()
+            if attachments:
+                self.instance.content = self.instance.save_new_attachments(attachments)
+        self.instance.clear_old_attachments()
 
 ImageAttachmentFormSet = inlineformset_factory(Article, ImageAttachment, form=ImageAttachmentForm, formset=BaseImageAttachmentFormSet, extra=0)
