479371508802130e99c1b9c52919e06bcd3c12f9
Peter Siemens <peterjsiemens@gmail.com>
2015-03-01 00:19:33 -0800

    adding user admin to manager

--- dispatch/apps/manager/urls.py
+++ dispatch/apps/manager/urls.py
@@ -6,7 +6,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include(admin.site.urls)),
-    url(r'articles/?', views.articles),
+    url(r'users/?', views.users),
+    url(r'user/(\d*)/?$', views.user_edit),
+    url(r'articles/?$', views.articles),
     url(r'article/(\d*)/?$', views.article_edit),
     url(r'article/delete/(\d*)/?$', views.article_delete),
     url(r'article/add/?$', views.article_add),
