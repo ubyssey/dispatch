3416b6206a3c19c7635b75b80f6992e7f27f8e7e
Peter Siemens <peterjsiemens@gmail.com>
2015-03-28 17:07:04 -0700

    playing around with components/templates

--- themes/ubyssey/templates.py
+++ themes/ubyssey/templates.py
@@ -1,9 +1,12 @@
 from dispatch.apps.frontend.helpers import templates
 
+class Homepage:
+    component_spots = ('multi-zone',)
 
 class FullWidth:
 
     NAME = 'Full width story'
     SLUG = 'fw-story'
 
+templates.register(Homepage)
 templates.register(FullWidth)
