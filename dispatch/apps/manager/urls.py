e53022c509b97071903a4d03a357f20fdcff1a93
Peter Siemens <peterjsiemens@gmail.com>
2015-04-06 17:48:05 -0700

    refactor manager urls

--- dispatch/apps/manager/urls.py
+++ dispatch/apps/manager/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from . import views
+from django.contrib.admin.views.decorators import staff_member_required
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include(admin.site.urls)),
-    url(r'users/?', views.users),
-    url(r'user/(\d*)/?$', views.user_edit),
-    url(r'articles/?$', views.articles),
-    url(r'article/(\d*)/?$', views.article_edit),
-    url(r'article/delete/(\d*)/?$', views.article_delete),
-    url(r'article/add/?$', views.article_add),
-    url(r'section/(?P<section>[-\w]+)/?', views.section),
+    url(r'^$', views.home),
+    url(r'^logout/?$', views.logout),
+    url(r'^login/?$', views.login),
+    url(r'^default/?$', include(admin.site.urls)),
+    url(r'^users/$', views.users),
+    url(r'^user/(\d*)/$', views.user_edit),
+    url(r'^articles/$', views.articles),
+    url(r'^article/(\d*)/$', views.article_edit),
+    url(r'^article/delete/(\d*)/?$', views.article_delete),
+    url(r'^article/add/?$', views.article_add),
+    url(r'^section/(?P<section>[-\w]+)/?$', views.section),
 )
