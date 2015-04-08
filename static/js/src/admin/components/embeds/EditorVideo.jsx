dcb6b3589be22fc010d44c3d12ee076f0c252b2e
Peter Siemens <peterjsiemens@gmail.com>
2015-04-07 19:26:21 -0700

    adding Youtube video embed type

---
+++ static/js/src/admin/components/embeds/EditorVideo.jsx
@@ -0,0 +1,62 @@
+var EditorVideo = React.createClass({
+    getInitialState: function(){
+        return {
+            validUrl: false,
+            url: this.props.data.url ? this.props.data.url : "",
+        };
+    },
+    validYT: function(url){
+      var p = /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?(?=.*v=((\w|-){11}))(?:\S+)?$/;
+      return (url.match(p)) ? RegExp.$1 : false;
+    },
+    handleURLChange: function(event){
+        event.preventDefault();
+        valid = this.validYT(event.target.value);
+        console.log(valid);
+        this.setState({
+            url: event.target.value,
+            validUrl: false,
+        });
+    },
+    getJSON: function(){
+        return {
+            type: 'youtube',
+            data: {
+                url: this.state.url,
+            }
+        }
+    },
+    renderInput: function(){
+        return (
+            <div>
+                <input placeholder="Enter a YouTube video URL" value={this.state.url} onChange={this.handleURLChange} />
+            </div>
+            )
+    },
+    renderVideo: function(){
+        return (
+            <div>
+                this is a video
+            </div>
+            )
+    },
+    render: function(){
+        return (
+            <div className="embed-video">
+            { this.state.validUrl ? this.renderVideo() : this.renderInput() }
+            </div>
+            );
+    }
+});
+
+
+var factory = {
+    controller: function(node, embed){
+        return React.render(
+            <EditorVideo data={embed.data} />,
+            node
+        );
+    },
+}
+
+module.exports = factory;
