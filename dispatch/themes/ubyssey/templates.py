1533b389142eb460198493d1621eecdd7bfea21e
Peter Siemens <peterjsiemens@gmail.com>
2015-04-17 15:37:29 -0700

    adding components/pages to Ubyssey theme

--- dispatch/themes/ubyssey/templates.py
+++ dispatch/themes/ubyssey/templates.py
@@ -1,12 +1,8 @@
 from dispatch.apps.frontend.helpers import templates
 
-class Homepage:
-    component_spots = ('multi-zone',)
-
 class FullWidth:
 
     NAME = 'Full width story'
     SLUG = 'fw-story'
 
-templates.register(Homepage)
 templates.register(FullWidth)
