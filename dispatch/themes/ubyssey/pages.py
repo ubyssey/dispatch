6cb3a91b288b6a722e14b991e1460429afbbc6f0
Peter Siemens <peterjsiemens@gmail.com>
2015-04-17 15:37:29 -0700

    adding components/pages to Ubyssey theme

---
+++ dispatch/themes/ubyssey/pages.py
@@ -0,0 +1,14 @@
+from dispatch.apps.frontend.helpers import theme_pages
+from dispatch.apps.frontend.components import BasePage
+
+class Homepage(BasePage):
+
+    NAME = 'Homepage'
+    SLUG = 'homepage'
+
+    component_spots = (
+        ('multi_zone', 'Multi-Zone'),
+        ('print_issue', 'Print Issue Box'),
+    )
+
+theme_pages.register(Homepage)
