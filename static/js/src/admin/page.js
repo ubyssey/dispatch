d9809a9da0dc2050c0f9c6601d4435bf4cba1cdc
Peter Siemens <peterjsiemens@gmail.com>
2015-04-17 15:39:48 -0700

    JS for component editor

---
+++ static/js/src/admin/page.js
@@ -0,0 +1,10 @@
+var PageEditor = require('./components/PageEditor.jsx');
+
+$(function(){
+    var container = $('#page-editor');
+    var slug = container.data('slug');
+    React.render(
+        <PageEditor slug={slug} />,
+        container.get(0)
+    );
+});
