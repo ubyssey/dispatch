ee765d8075354b6b45bfad93c35853c360507e8c
Peter Siemens <peterjsiemens@gmail.com>
2015-04-21 10:12:06 -0700

    cleaning & documenting API code

--- dispatch/apps/api/urls.py
+++ dispatch/apps/api/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
+
 from dispatch.apps.api import views
 
 router = routers.DefaultRouter()
@@ -8,7 +10,6 @@ router = routers.DefaultRouter()
 router.register(r'articles', views.ArticleViewSet, base_name='articles')
 router.register(r'frontpage', views.FrontpageViewSet, base_name='frontpage')
 router.register(r'sections', views.SectionViewSet, base_name='sections')
-router.register(r'users', views.UserViewSet)
 router.register(r'person', views.PersonViewSet, base_name='person')
 router.register(r'tag', views.TagViewSet)
 router.register(r'images', views.ImageViewSet, base_name='images')
@@ -18,9 +19,14 @@ section_frontpage = views.SectionViewSet.as_view({
     'get': 'frontpage',
 })
 
+component = views.ComponentViewSet.as_view({
+    'get': 'detail',
+    'post': 'update',
+})
+
 urlpatterns = format_suffix_patterns([
     # Extra section routes
     url(r'^sections/(?P<pk>[0-9]+)/frontpage/$', section_frontpage, name='section-frontpage'),
     url(r'^sections/(?P<slug>[\w-]+)/frontpage/$', section_frontpage, name='section-frontpage'),
-    url(r'^components/(?P<slug>[\w-]+)/$', views.component_view, name='component'),
+    url(r'^components/(?P<slug>[\w-]+)/$', component, name='component'),
 ]) + router.urls
