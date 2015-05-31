105aa87697d7da3c0ecd27f43b67f3f296b5a633
Peter Siemens <peterjsiemens@gmail.com>
2015-05-31 17:59:32 -0600

    modifying embed controllers to take arguments

--- static/js/src/admin/components/embeds/EditorImage.jsx
+++ static/js/src/admin/components/embeds/EditorImage.jsx
@@ -1,8 +1,12 @@
+var React = require('react');
 var EditorImage = React.createClass({
     getInitialState: function(){
         return {
             type: 'single',
-            images: this.props.data.images,
+            image: {
+                id: this.props.data.id,
+                url: this.props.data.url,
+            },
             caption: this.props.data.caption ? this.props.data.caption : '',
         };
     },
@@ -10,33 +14,15 @@ var EditorImage = React.createClass({
         $(React.findDOMNode(this.refs.captionTextarea)).autosize();
     },
     addImage: function(image){
-        if (this.state.images.length > 1)
-            return
-        var image = {
-            id: image.id,
-            src: image.url,
-        }
-        var images = this.state.images;
-        images.push(image);
+        console.log(image);
         this.setState({
-            images: images,
+            image: image
         });
     },
     openImageManager: function(){
         this.props.manager.openWithCallback(function(items){
             this.addImage(items[0]);
         }.bind(this));
-    },
-    toggleType: function(e){
-        e.preventDefault();
-        var newType;
-        if (this.state.type == 'single')
-            newType = 'double';
-        else
-            newType = 'single';
-        this.setState({
-            type: newType,
-        });
     },
     handleCaptionChange: function(event){
         this.setState({
@@ -49,30 +35,20 @@ var EditorImage = React.createClass({
             data: {
                 attachment_id: this.props.data.attachment_id ? this.props.data.attachment_id : false,
                 subtype: this.state.type,
-                images: this.state.images,
+                image: this.state.image,
                 caption: this.state.caption,
             }
         }
     },
     render: function(){
-        var imageNodes = this.state.images.map(function (image) {
-            return (
-                <img className="item" key={image.id} src={image.src} />
-                );
-        });
-        var showAddButton = this.state.images.length == 1 && this.state.type == 'double' ? 'show-add-button' : '';
         return (
             <div className="image">
                 <div className="image-toolbar-container">
                     <div className="image-toolbar">
-                        <a href="#" onClick={this.toggleType}>Type</a>
                     </div>
                 </div>
-                <div className={'images ' + this.state.type + ' ' + showAddButton}>
-                    {imageNodes}
-                    <div className="add-image" ref="addImage">
-                        <a href="#" onClick={this.openImageManager}>Add Image</a>
-                    </div>
+                <div className="images">
+                    <img onClick={this.openImageManager} className="item" key={this.state.image.id} src={this.state.image.url} />
                 </div>
                 <div className="image-caption">
                     <textarea placeholder="Write a caption" ref="captionTextarea" onChange={this.handleCaptionChange}>{this.state.caption}</textarea>
@@ -83,17 +59,9 @@ var EditorImage = React.createClass({
 });
 
 
-var factory = function(manager, images){
+var factory = function(options){
+    var manager = options.manager;
     var controller = function(node, embed){
-        if(typeof embed.data.images === 'undefined'){
-            var id = embed.data.attachment_id;
-            var attachment = images[id];
-            embed.data.images = [{
-                id: attachment.image.id,
-                src: attachment.image.url
-            }];
-            embed.data.caption = attachment.caption;
-        }
         var component = React.render(
             <EditorImage data={embed.data} manager={manager} />,
             node
@@ -106,10 +74,8 @@ var factory = function(manager, images){
             manager.openWithCallback(function(items){
                 var image = items[0];
                 var data = {
-                    'images': [{
-                        id: image.id,
-                        src: image.url,
-                    }]
+                    id: image.id,
+                    url: image.url,
                 }
                 callback(data);
             });
