518a8d65ae08412dd175a60994c582b76e92254d
Peter Siemens <peterjsiemens@gmail.com>
2015-05-31 14:00:55 -0600

    API: change "person" route to "people"

--- dispatch/api/urls.py
+++ dispatch/api/urls.py
@@ -10,7 +10,7 @@ router = routers.DefaultRouter()
 router.register(r'articles', views.ArticleViewSet, base_name='articles')
 router.register(r'frontpage', views.FrontpageViewSet, base_name='frontpage')
 router.register(r'sections', views.SectionViewSet, base_name='sections')
-router.register(r'person', views.PersonViewSet, base_name='person')
+router.register(r'people', views.PersonViewSet, base_name='person')
 router.register(r'tag', views.TagViewSet)
 router.register(r'images', views.ImageViewSet, base_name='images')
 
