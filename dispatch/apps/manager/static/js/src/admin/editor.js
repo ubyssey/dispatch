ac241443edc3fd9b9e74ca9f2397e82c14187038
Peter Siemens <peterjsiemens@gmail.com>
2015-06-10 20:42:14 -0600

    move static directory out of manager app

--- dispatch/apps/manager/static/js/src/admin/editor.js
+++
@@ -1,6 +0,0 @@
-var CSRF_TOKEN = $(".article-form").data('csrf');
-
-var React = require('react');
-var EditorImage = require('./components/embeds/EditorImage.jsx');
-var EditorCode = require('./components/embeds/EditorCode.jsx');
-var EditorVideo = require('./components/embeds/EditorVideo.jsx');
