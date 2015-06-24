297396dc30e5b85cdaf1e2a0686f4c5e4512806f
Peter Siemens <peterjsiemens@gmail.com>
2015-06-24 00:40:08 -0600

    remove old admin files

--- dispatch/apps/frontend/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from dispatch.apps.frontend.models import Snippet, Script, Stylesheet
-
-admin.site.register(Snippet)
-admin.site.register(Script)
-admin.site.register(Stylesheet)
