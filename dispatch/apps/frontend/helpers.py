01576da97a9331e87c258743c8a8d52dc1f9d77b
Peter Siemens <peterjsiemens@gmail.com>
2015-06-24 00:41:21 -0600

    allow for template overriding

--- dispatch/apps/frontend/helpers.py
+++ dispatch/apps/frontend/helpers.py
@@ -6,19 +6,20 @@ def print_templates(name):
 class TemplateRegistry:
 
     def __init__(self):
-        self.templates = []
+        self.template_list = []
+        self.templates = {}
 
     def register(self, template_class):
-        self.templates.append(template_class)
+        if template_class.SLUG in self.template_list:
+            self.template_list.remove(template_class.SLUG)
+        self.template_list.append(template_class.SLUG)
+        self.templates[template_class.SLUG] = template_class
 
     def get(self, slug):
-        for template in self.templates:
-            if template.SLUG == slug:
-                return template
-        return None
+        return self.templates[slug]
 
     def all(self):
-        return self.templates
+        return [self.templates[slug] for slug in self.template_list]
 
 class ComponentRegistry(TemplateRegistry):
     def get_for_spot(self, spot):
