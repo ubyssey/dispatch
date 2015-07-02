c085b986df1b9a78559b89440c604508a719a828
Peter Siemens <peterjsiemens@gmail.com>
2015-07-02 15:17:00 -0600

    improve custom JSONField

--- dispatch/api/fields.py
+++ dispatch/api/fields.py
@@ -5,3 +5,5 @@ from rest_framework import serializers
 class JSONField(serializers.Field):
     def to_internal_value(self, value):
         return json.loads(value)
+    def to_representation(self, value):
+        return value
