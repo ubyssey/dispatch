a9d0944b4c4e86ea0c31ca525ed44e4d34d2767c
Peter Siemens <peterjsiemens@gmail.com>
2015-07-07 11:06:59 -0600

    add lists to text editor

--- static/manager/js/src/components/QuillEditorToolbar.jsx
+++ static/manager/js/src/components/QuillEditorToolbar.jsx
@@ -6,7 +6,9 @@ var toolbar = (
             <span title="Bold" className="ql-format-button ql-bold"><i className="fa fa-bold"></i></span>
             <span title="Italic" className="ql-format-button ql-italic"><i className="fa fa-italic"></i></span>
             <span title="Underline" className="ql-format-button ql-underline"><i className="fa fa-underline"></i></span>
-            <span title="Strikethrough" className="ql-format-button ql-strike"><i className="fa fa-strikethrough"></i></span>
+        </span>
+        <span className="ql-format-group">
+            <span title="Bullet" className="ql-format-button ql-bullet"><i className="fa fa-list-ul"></i></span>
         </span>
     </div>
     );
