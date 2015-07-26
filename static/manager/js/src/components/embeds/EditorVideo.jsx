cbafe60f55b5be6ba98f18858717c94dda8bc977
Peter Siemens <peterjsiemens@gmail.com>
2015-07-26 16:31:49 -0600

    improve EditorVideo component

--- static/manager/js/src/components/embeds/EditorVideo.jsx
+++ static/manager/js/src/components/embeds/EditorVideo.jsx
@@ -3,7 +3,13 @@ var EditorVideo = React.createClass({
     getInitialState: function(){
         return {
             validUrl: false,
+            id: this.props.data.id,
             url: this.props.data.url ? this.props.data.url : "",
+            inserted: this.props.data.url ? true : false,
+            validUrl: this.props.data.url ? true : false,
+            title: this.props.data.title,
+            caption: this.props.data.caption,
+            credit: this.props.data.credit
         };
     },
     validYT: function(url){
@@ -12,41 +18,80 @@ var EditorVideo = React.createClass({
     },
     handleURLChange: function(event){
         event.preventDefault();
-        valid = this.validYT(event.target.value);
-        console.log(valid);
+        var id = this.validYT(event.target.value);
         this.setState({
             url: event.target.value,
-            validUrl: false,
+            id: id,
+            validUrl: id ? true : false
         });
+    },
+    insertVideo: function(){
+        this.setState({ inserted: true });
+    },
+    removeVideo: function(event){
+        event.preventDefault();
+        this.props.remove();
+    },
+    startEditing: function(){
+        this.setState({ inserted: false });
+    },
+    updateTitle: function(event){
+        this.setState({ title: event.target.value });
+    },
+    updateCaption: function(event){
+        this.setState({ caption: event.target.value });
+    },
+    updateCredit: function(event){
+        this.setState({ credit: event.target.value });
     },
     getJSON: function(){
         return {
-            type: 'youtube',
+            type: 'video',
             data: {
+                source: 'youtube',
+                id: this.state.id,
                 url: this.state.url,
+                title: this.state.title,
+                caption: this.state.caption,
+                credit: this.state.credit
             }
         }
     },
     renderInput: function(){
         return (
-            <div>
+            <div className="insert-video">
                 <input placeholder="Enter a YouTube video URL" value={this.state.url} onChange={this.handleURLChange} />
+                <button className="dis-button green" disabled={!this.state.validUrl} onClick={this.insertVideo}>Insert</button>
+                <button className="dis-button" onClick={this.removeVideo}>Cancel</button>
             </div>
             )
     },
     renderVideo: function(){
         return (
-            <div>
-                this is a video
-            </div>
-            )
-    },
-    render: function(){
-        return (
-            <div className="embed-video">
-            { this.state.validUrl ? this.renderVideo() : this.renderInput() }
+            <div className="video basic">
+                <img src={"http://img.youtube.com/vi/" + this.state.id + "/0.jpg"} />
+                <div>
+                    <div className="field full">
+                        <label>Title</label>
+                        <input type="text" onChange={this.updateTitle} value={this.state.title} />
+                    </div>
+                    <div className="field full">
+                        <label>Caption</label>
+                        <textarea onChange={this.updateCaption} value={this.state.caption}></textarea>
+                    </div>
+                    <div className="field full">
+                        <label>Credit</label>
+                        <input type="text" onChange={this.updateCredit} value={this.state.credit} />
+                    </div>
+                </div>
+                <div className="pull-right">
+                    <button onClick={this.removeVideo}>Remove</button>
+                </div>
             </div>
             );
+    },
+    render: function(){
+        return (<div className="embed-video">{ this.state.inserted ? this.renderVideo() : this.renderInput() }</div>);
     }
 });
 
@@ -55,7 +100,7 @@ var factory = function(options){
     return {
         controller: function(line, embed){
             return React.render(
-                <EditorVideo data={embed.data} />,
+                <EditorVideo data={embed.data} remove={line.remove} />,
                 line.node
             );
         },
