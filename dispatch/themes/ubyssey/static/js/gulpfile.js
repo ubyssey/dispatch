31f7f9e8e56b3c6d012dd8986f501cc7dc69f022
Peter Siemens <peterjsiemens@gmail.com>
2015-07-31 13:56:56 -0600

    new gulpfile for article js

--- dispatch/themes/ubyssey/static/js/gulpfile.js
+++ dispatch/themes/ubyssey/static/js/gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var react = require('gulp-react');
 var shell = require('gulp-shell');
 
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var browserify = require('browserify');
+var watchify = require('watchify');
+
+var reactify = require('reactify');
+var uglify = require('gulp-uglify');
+var derequire = require('gulp-derequire');
+
 gulp.task('default', function () {
     return gulp.src('src/article.jsx')
         .pipe(react())
@@ -14,3 +24,17 @@ gulp.task('comments', function () {
         .pipe(gulp.dest('dist'))
         .pipe(shell('python ../../../../../manage.py collectstatic -i node_modules --noinput'));
 });
+
+gulp.task('article', function(){
+    return browserify('./src/article.jsx')
+        .transform({ global: true }, reactify)
+        .bundle()
+        .pipe(source('article.js'))
+        .pipe(derequire())
+        //.pipe(buffer())
+        //.pipe(uglify())
+        .pipe(gulp.dest('dist'))
+        .pipe(shell('python ../../../../../manage.py collectstatic -i node_modules --noinput'));
+});
+
+gulp.task('static', shell.task('python ../../../../../manage.py collectstatic -i node_modules --noinput'));
