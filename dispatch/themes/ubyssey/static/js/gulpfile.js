29ae8463f06d501ab4fe75990d0580d34728a223
Peter Siemens <peterjsiemens@gmail.com>
2015-07-31 10:35:02 -0600

    add comments to gulpfile

--- dispatch/themes/ubyssey/static/js/gulpfile.js
+++ dispatch/themes/ubyssey/static/js/gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
+var shell = require('gulp-shell');
 
 gulp.task('default', function () {
     return gulp.src('src/article.jsx')
         .pipe(react())
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('comments', function () {
+    return gulp.src('src/comments.jsx')
+        .pipe(react())
+        .pipe(gulp.dest('dist'))
+        .pipe(shell('python ../../../../../manage.py collectstatic -i node_modules --noinput'));
 });
