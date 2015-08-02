8a98ebc93b045ad6a310f18563dfa41fe1a79c25
Peter Siemens <peterjsiemens@gmail.com>
2015-08-02 15:27:27 -0600

    better article loading/storage/tracking

--- dispatch/themes/ubyssey/static/js/src/components/ArticleList.jsx
+++ dispatch/themes/ubyssey/static/js/src/components/ArticleList.jsx
@@ -2,6 +2,9 @@ var React = require('react');
 
 var Article = require('./Article.jsx');
 var CommentsBar = require('./CommentsBar.jsx');
+var ArticleHeader = require('./ArticleHeader.jsx');
+
+
 var LinkedList = function(array){
 
     var Node = function(id){
@@ -23,6 +26,7 @@ var LinkedList = function(array){
 
 }
 
+
 var ArticleList = React.createClass({
     getInitialState: function(){
         var articles = this.props.articles;
@@ -30,14 +34,21 @@ var ArticleList = React.createClass({
 
         return {
             active: LinkedList(articles),
+            articles: [this.props.firstArticle],
             loading: false
         }
     },
+    componentWillMount: function(){
+        this.articlesTable = {};
+        this.articlesTable[this.props.firstArticle.id] = 0;
+    },
     componentDidMount: function(){
-        this.loaded = [this.props.firstArticle];
-        this.unloaded = true;
+        this.loaded = [this.props.firstArticle.id];
         this.afterLoad = null;
+
         this.scrollListener();
+        if(this.state.active.next)
+            this.loadNext(this.state.active.next.id);
     },
     updateHeader: function(topPos){
 
@@ -50,6 +61,10 @@ var ArticleList = React.createClass({
             $('.header-article').hide();
             $('.header-site').show();
         }
+
+    },
+    getArticle: function(id){
+        return this.state.articles[this.articlesTable[id]];
     },
     getArticlePoints: function(){
         var $article = $('#article-'+this.state.active.id);
@@ -60,33 +75,42 @@ var ArticleList = React.createClass({
             top: top,
             mid: Math.round(end - (height / 2)),
             end: end,
+            height: height
         }
     },
     scrollListener: function(){
         var windowHeight = $(window).height();
-        var updateScroll = function(document){
+        var documentHeight = $(document).height();
 
-            var scrollPos = $(document).scrollTop() + windowHeight;
-            var points = this.getArticlePoints();
+        var cachedPoints;
+        var points;
+        var timer = 0;
 
-            if(scrollPos > (points.mid - 40) && scrollPos < (points.mid + 40)){
-                this.loadNext();
+        var updateScroll = function(){
+
+            var topPos = $(document).scrollTop();
+            var bottomPos = topPos + windowHeight;
+
             this.updateHeader(topPos);
+
+            if(cachedPoints != this.state.active.id){
+                points = this.getArticlePoints();
+                cachedPoints = this.state.active.id;
             }
 
-            if(scrollPos > points.end){
+            if(bottomPos > points.end)
+                this.prepNext();
+
+            if(topPos > points.end + 50 || (points.height < windowHeight && bottomPos > (documentHeight - 50)))
                 this.setNext();
-            }
 
-            if(scrollPos < points.top){
+            if(bottomPos < points.top - 50)
                 this.setPrev();
-            }
 
         }.bind(this);
 
-        $(document).scroll(function() {
-            updateScroll(this);
-        });
+        $(window).scroll(updateScroll);
+
     },
     setPrev: function(){
         if(!this.state.active.prev)
@@ -94,15 +118,19 @@ var ArticleList = React.createClass({
 
         this.setState({ active: this.state.active.prev }, this.updateURL);
     },
+    prepNext: function(){
+        if(!this.state.active.next || !this.state.active.next.next)
+            return
+        if(!this.isLoaded(this.state.active.next.next.id))
+            this.loadNext(this.state.active.next.next.id);
     },
     setNext: function(){
 
-        if(!this.state.active.next){
+        if(!this.state.active.next)
             return;
-        }
 
         if(!this.isLoaded(this.state.active.next.id)){
-            this.loadNext();
+            this.loadNext(this.state.active.next.id);
             this.afterLoad = this.setNext;
             return;
         }
@@ -114,17 +142,13 @@ var ArticleList = React.createClass({
 
         this.setState({ active: this.state.active.next }, this.updateURL);
     },
-    loadNext: function(){
-
-        if(!this.state.active.next || this.state.loading || this.isLoaded(this.state.active.next.id)){
     updateURL: function(){
         history.pushState(null, null, this.getArticle(this.state.active.id).url);
     },
+    loadNext: function(article_id){
+        if(this.state.loading || this.isLoaded(article_id))
             return;
-        }
-
-        this.loadArticle(this.state.active.next.id);
-
+        this.loadArticle(article_id);
     },
     isLoaded: function(id){
         var id = parseInt(id);
@@ -132,17 +156,21 @@ var ArticleList = React.createClass({
     },
     loadArticle: function(article_id){
         this.setState({ loading: true });
-        dispatch.articleRendered(article_id, function(html){
+        dispatch.articleRendered(article_id, function(data){
             this.loaded.push(parseInt(article_id));
-            this.renderArticle(html);
+            this.renderArticle(data);
         }.bind(this));
     },
-    renderArticle: function(html){
+    renderArticle: function(data){
         var articles = this.state.articles;
-        articles.push(html);
+        articles.push(data);
+
         this.setState({ loading: false, articles: articles }, function(){
+
+            this.articlesTable[data.id] = articles.length - 1;
+
             if(!this.afterLoad){
-                return;
+                return
             }
             this.afterLoad();
             this.afterLoad = null;
