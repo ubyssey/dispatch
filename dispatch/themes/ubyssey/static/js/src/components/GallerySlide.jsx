f595ed24481e12e77e55a12348e9df713b54c01b
Peter Siemens <peterjsiemens@gmail.com>
2015-08-05 14:23:54 -0600

    trying new swipe events

--- dispatch/themes/ubyssey/static/js/src/components/GallerySlide.jsx
+++ dispatch/themes/ubyssey/static/js/src/components/GallerySlide.jsx
@@ -2,12 +2,12 @@ var React = require('react');
 
 var GallerySlide = React.createClass({
     render: function(){
-        var imageStyle = { height: $(window).height() };
+        var slideStyle = { width: this.props.width, height: $(window).height() };
         return (
-            <div className={"slide " + this.props.className}>
-                <img className="slide-image" style={imageStyle} src={this.props.src} />
+            <li className={"slide " + this.props.className} style={slideStyle} >
+                <img className="slide-image" src={this.props.src} />
                 <p className="slide-caption">{this.props.caption}</p>
-            </div>
+            </li>
         );
     }
 });
