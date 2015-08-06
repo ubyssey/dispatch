8659bc1526a9c4ea3071607dc1deee996d90f6d9
Pippin Lee <pippinblee@gmail.com>
2015-08-06 01:25:13 -0400

    update indicators for tab from headline and slug

--- static/manager/js/src/components/ArticleAdmin.jsx
+++ static/manager/js/src/components/ArticleAdmin.jsx
@@ -357,7 +357,7 @@ var ArticleAdmin = React.createClass({
                                 <TemplateEditor ref="template" article_id={this.state.article.id} fields={this.state.article.template_fields} template={this.state.article.template} updateHandler={this.updateField} />
                             </TabPanel>
                             <TabPanel>
-                                <SEO keyword={this.state.article.seo_keyword} snippet={this.state.article.seo_snippet} updateHandler={this.updateField} />
+                                <SEO keyword={this.state.article.seo_keyword} article={this.state.article} snippet={this.state.article.seo_snippet} updateHandler={this.updateField} />
                             </TabPanel>
                         </Tabs>
                     </div>
