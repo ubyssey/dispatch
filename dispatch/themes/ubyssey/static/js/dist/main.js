a8b7cd07433603dceecb770b8e87d32e1700cb24
Peter Siemens <peterjsiemens@gmail.com>
2015-08-26 16:14:17 -0600

    twitter + Facebook links

--- dispatch/themes/ubyssey/static/js/dist/main.js
+++ dispatch/themes/ubyssey/static/js/dist/main.js
@@ -12,3 +12,20 @@ $('.dropdown > a').click(function(e){
 $(document).on('click', function(){
     $('.dropdown ul').hide();
 });
+
+
+$('a.facebook').click(function(e){
+    e.preventDefault();
+    FB.ui({
+      method: 'share_open_graph',
+      action_type: 'og.likes',
+      action_properties: JSON.stringify({
+          object: $(this).data('url'),
+      })
+    }, function(response){});
+});
+
+$('a.twitter').click(function(e){
+  e.preventDefault();
+  window.open('http://twitter.com/share?url=' + $(this).data('url') + '&text=' + $(this).data('title') + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+($(window).width()/2 - 225) +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+});
