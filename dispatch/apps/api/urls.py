d2bb2d30efe7f7df1940cb4336f36063af0c7e93
Peter Siemens <peterjsiemens@gmail.com>
2015-09-09 18:27:53 -0700

    spotlight search component

--- dispatch/apps/api/urls.py
+++ dispatch/apps/api/urls.py
@@ -17,7 +17,6 @@ router.register(r'images', views.ImageViewSet, base_name='images')
 router.register(r'galleries', views.ImageGalleryViewSet, base_name='galleries')
 router.register(r'templates', views.TemplateViewSet, base_name='templates')
 router.register(r'comments', views.CommentViewSet, base_name='comments')
-
 router.register(r'trending', views.TrendingViewSet, base_name='trending')
 
 section_frontpage = views.SectionViewSet.as_view({
