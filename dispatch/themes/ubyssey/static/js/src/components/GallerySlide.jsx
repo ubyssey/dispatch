b4c676d97303b20adf564e46a7931010dff9c68c
Peter Siemens <peterjsiemens@gmail.com>
2015-09-12 10:54:46 -0700

    fixing image gallery

--- dispatch/themes/ubyssey/static/js/src/components/GallerySlide.jsx
+++ dispatch/themes/ubyssey/static/js/src/components/GallerySlide.jsx
@@ -2,15 +2,25 @@ var React = require('react');
 
 var GallerySlide = React.createClass({
     render: function(){
-        var slideStyle = { width: this.props.width, height: $(window).height() };
+        var slideStyle = { width: $(window).width() };
+        var imageStyle = {};
+        if($(window).height() > $(window).width())
+            imageStyle = { width: '100%', height: 'auto' };
+
+        var caption = (<p className="slide-caption">{this.props.caption}</p>);
         return (
-            <li className="slide" style={slideStyle} >
-                <img className="slide-image" src={this.props.src} />
-                <p className="slide-caption">{this.props.caption}</p>
+            <li className="slide" style={slideStyle}>
+                <div className="inner">
+                    <div className="image">
+                        <div>
+                            <img src={this.props.src} style={imageStyle} />
+                        </div>
+                    </div>
+                    { this.props.caption ? caption : null}
+                </div>
             </li>
         );
     }
 });
-
 
 module.exports = GallerySlide;
