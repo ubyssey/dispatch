8a477d495f81ce93c899f01d31e2edc176726f0d
Peter Siemens <peterjsiemens@gmail.com>
2015-09-25 00:20:56 -0700

    fix theme urls

--- dispatch/themes/ubyssey/urls.py
+++ dispatch/themes/ubyssey/urls.py
@@ -4,9 +4,15 @@ from dispatch.apps.frontend.themes.default.urls import theme_urls
 
 theme = UbysseyTheme()
 
-theme_urls = theme_urls(theme) + patterns('',
-    url(r'^$', theme.home, name='home'),
+theme_urls = patterns('',
     url(r'^search/?', theme.search, name='search'),
+    url(r'^(?P<section>[-\w]+)/(?P<slug>[-\w]+)/?', theme.article, name='article'),
+    url(r'^(?P<slug>[-\w]+)/?', theme.section, name='section'),
+    #url(r'^login/?', theme.login),
+    #url(r'^logout/?', theme.logout),
+    #url(r'^register/?', theme.register),
     url(r'^author/(?P<slug>[-\w]+)/articles/?', theme.author_articles, name='author-articles'),
     url(r'^author/(?P<slug>[-\w]+)/?', theme.author, name='author'),
+    url(r'^$', theme.home, name='home'),
+
 )
