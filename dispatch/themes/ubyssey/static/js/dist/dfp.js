3b9527855f7e518e263b203c3623d1acb1752a9a
Peter Siemens <peterjsiemens@gmail.com>
2015-09-26 14:21:22 -0700

    pages and styles

--- dispatch/themes/ubyssey/static/js/dist/dfp.js
+++ dispatch/themes/ubyssey/static/js/dist/dfp.js
@@ -4,26 +4,36 @@ var sizes = {
     'mobile-leaderboard': [300, 50]
 };
 
+function loadAdvertisements(){
+    var dfpslots=$(document).find(".adslot").filter(":visible"),
+    i=0,
+    slot = new Array();
+
+
+
+    if (dfpslots.length) {
+        googletag.cmd.push(function() {
+            $(dfpslots).each(function(){
+                console.log($(this).attr('id'));
+                slot[i] = googletag.defineSlot('/61222807/'+$(this).data('dfp'), sizes[$(this).data('size')], $(this).attr('id')).addService(googletag.pubads());
+                i++
+            });
+
+            //googletag.pubads().enableSingleRequest(); // Breaks channel reporting
+            googletag.enableServices();
+
+            $(dfpslots).each(function(){
+                console.log($(this).attr('id'));
+                googletag.display($(this).attr('id'));
+            });
+        });
+    }
+};
+
+function refreshAdvertisements(){
+    loadAdvertisements();
+};
+
 $(document).ready(function(){
-	var dfpslots=$("body").find(".adslot").filter(":visible"),
-	i=0,
-	slot = new Array();
-
-	if (dfpslots.length) {
-		googletag.cmd.push(function() {
-			$(dfpslots).each(function(){
-				slot[i] = googletag.defineSlot('/61222807/'+$(this).data('dfp'), sizes[$(this).data('size')], $(this).attr('id')).addService(googletag.pubads());
-				i++
-			});
-
-			//googletag.pubads().enableSingleRequest(); // Breaks channel reporting
-			googletag.enableServices();
-
-			$(dfpslots).each(function(){
-				googletag.display($(this).attr('id'));
-			});
-		});
-	}
-    // Collapse empty ad divs
-    googletag.pubads().collapseEmptyDivs();
+    loadAdvertisements();
 });
