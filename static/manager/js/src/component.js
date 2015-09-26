291c44b682cfda718830cc6ea744e81b7ce61afe
Peter Siemens <peterjsiemens@gmail.com>
2015-09-26 02:15:01 -0700

    this is the right component code

--- static/manager/js/src/component.js
+++ static/manager/js/src/component.js
@@ -2,7 +2,7 @@ var React = require('react');
 var PageEditor = require('./components/PageEditor.jsx');
 
 $(function(){
-    var container = $('#page-editor');
+    var container = $('#component-editor');
     var slug = container.data('slug');
     React.render(
         <PageEditor slug={slug} />,
