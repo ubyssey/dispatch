ad55fee586f504f47228273e73012d55757245ec
Ben Cook <b@bencook.ca>
2015-09-26 20:06:35 -0700

    Searchbar: cache some $vars, then wrap in IIFE for scoping

--- dispatch/themes/ubyssey/static/js/dist/main.js
+++ dispatch/themes/ubyssey/static/js/dist/main.js
@@ -1,60 +1,64 @@
-$('.dropdown > a').click(function(e){
-    e.preventDefault();
-    var dropdown = $(this).parent().find('ul');
-    if(dropdown.is(':visible')){
-        dropdown.hide();
-    } else {
-        dropdown.show();
-    }
-    return false;
-});
+(function() {
+    var $searchform = $('#search-form');
 
-$(document).on('click', function(){
-    $('.dropdown ul').hide();
-});
-
-$('a.menu').click(function(e){
-    e.preventDefault();
-    if($('nav.mobile').is(':visible')){
-        $('nav.mobile').hide();
-        $(this).removeClass('active');
-    } else {
-        if($('#search-form').is(':visible')){
-            $('#search-form').hide();
-            $('a.search').removeClass('active');
+    $('.dropdown > a').click(function(e){
+        e.preventDefault();
+        var dropdown = $(this).parent().find('ul');
+        if(dropdown.is(':visible')){
+            dropdown.hide();
+        } else {
+            dropdown.show();
         }
-        $('nav.mobile').show();
-        $(this).addClass('active');
-    }
-});
+        return false;
+    });
 
-$('a.search').click(function(e){
-    e.preventDefault();
-    if($('#search-form').is(':visible')){
-        $('#search-form').hide();
-        $(this).removeClass('active');
-    } else {
+    $(document).on('click', function(){
+        $('.dropdown ul').hide();
+    });
+
+    $('a.menu').click(function(e){
+        e.preventDefault();
         if($('nav.mobile').is(':visible')){
             $('nav.mobile').hide();
-            $('a.menu').removeClass('active');
+            $(this).removeClass('active');
+        } else {
+            if($searchform.is(':visible')){
+                $searchform.hide();
+                $('a.search').removeClass('active');
+            }
+            $('nav.mobile').show();
+            $(this).addClass('active');
         }
-        $('#search-form').show();
-        $(this).addClass('active');
-    }
-});
+    });
 
-$('a.facebook').click(function(e){
-    e.preventDefault();
-    FB.ui({
-      method: 'share_open_graph',
-      action_type: 'og.likes',
-      action_properties: JSON.stringify({
-          object: $(this).data('url'),
-      })
-    }, function(response){});
-});
+    $('a.search').click(function(e){
+        e.preventDefault();
+        if($searchform.is(':visible')){
+            $searchform.hide();
+            $(this).removeClass('active');
+        } else {
+            if($('nav.mobile').is(':visible')){
+                $('nav.mobile').hide();
+                $('a.menu').removeClass('active');
+            }
+            $searchform.show();
+            $(this).addClass('active');
+        }
+    });
 
-$('a.twitter').click(function(e){
-  e.preventDefault();
-  window.open('http://twitter.com/share?url=' + $(this).data('url') + '&text=' + $(this).data('title') + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+($(window).width()/2 - 225) +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
-});
+    $('a.facebook').click(function(e){
+        e.preventDefault();
+        FB.ui({
+          method: 'share_open_graph',
+          action_type: 'og.likes',
+          action_properties: JSON.stringify({
+              object: $(this).data('url'),
+          })
+        }, function(response){});
+    });
+
+    $('a.twitter').click(function(e){
+      e.preventDefault();
+      window.open('http://twitter.com/share?url=' + $(this).data('url') + '&text=' + $(this).data('title') + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+($(window).width()/2 - 225) +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    });
+})();
