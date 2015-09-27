8f8c8c38ef237ff7e6c6238c6df441abdb46c4e1
Jordan Schalm <jordan.schalm@gmail.com>
2015-09-27 14:59:19 -0700

    commit settings

--- dispatch/settings.py
+++ dispatch/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dispatch',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'jmacs',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
