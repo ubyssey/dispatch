8a9870dc8374d994c290787165d3d77f18cfcfa6
Peter Siemens <peterjsiemens@gmail.com>
2016-03-15 22:52:36 -0700

    Serve static file using development server

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework import routers
 
@@ -12,3 +14,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(ThemeHelper.get_theme_urls())),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
