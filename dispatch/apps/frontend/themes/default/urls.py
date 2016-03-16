22ae568a77bb2be76296044c04e534b5e6d4db10
Peter Siemens <peterjsiemens@gmail.com>
2016-03-15 22:52:20 -0700

    Store urls in normal list

--- dispatch/apps/frontend/themes/default/urls.py
+++ dispatch/apps/frontend/themes/default/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from views import DefaultTheme
 
 theme = DefaultTheme()
 
-theme_urls = patterns('',
+theme_urls = [
     url(r'^(?P<section>[-\w]+)/(?P<slug>[-\w]+)/?', theme.article, name='article'),
     url(r'^(?P<slug>[-\w]+)/?', theme.section, name='section'),
     url(r'^login/?', theme.login),
     url(r'^logout/?', theme.logout),
     url(r'^register/?', theme.register),
     url(r'^/', theme.home),
-)
+]
