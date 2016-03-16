d3f0f675fd6c3c2343e926e8ebb08a815dc85878
Peter Siemens <peterjsiemens@gmail.com>
2016-03-15 22:57:18 -0700

    Standardize URL naming

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import include, url
 
 from rest_framework import routers
 
-from dispatch.apps.api.urls import urlpatterns as api_patterns
 from dispatch.helpers.theme import ThemeHelper
-from dispatch.apps.manager import urls as adminurls
+from dispatch.apps.api.urls import urlpatterns as api_urls
+from dispatch.apps.manager.urls import urlpatterns as admin_urls
 
 urlpatterns = [
-    url(r'^admin/', include(adminurls)),
-    url(r'^api/', include(api_patterns)),
+    url(r'^admin/', include(admin_urls)),
+    url(r'^api/', include(api_urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(ThemeHelper.get_theme_urls())),
 ]
