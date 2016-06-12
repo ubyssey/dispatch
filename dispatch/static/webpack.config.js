e288b7424ca74db2528bae93f62871aa6e97b5ec
Peter Siemens <peterjsiemens@gmail.com>
2016-06-12 14:13:37 -0500

    Add dispatch.js to webpack

--- dispatch/static/webpack.config.js
+++ dispatch/static/webpack.config.js
@@ -2,22 +2,23 @@ var webpack = require('webpack');
 var version = require('./package.json').version;
 
 module.exports = {
-    entry: {
-        article:   './src/js/article.jsx',
-        component: './src/js/component.jsx',
-        list:      './src/js/list.jsx'
-    },
-    output: {
-        path: __dirname + '/dist/js',
-        filename: '[name]-' + version + '.js'
-    },
-    module: {
-      loaders: [
-        {test: /\.jsx|.js$/, include: __dirname + '/src/js', loader: 'babel-loader'},
-        {test: /\.jsx|.js$/, include: __dirname + '/node_modules/react-tabs', loader: 'babel-loader'}
-      ]
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin()
+  entry: {
+    dispatch:  './src/js/dispatch.js',
+    article:   './src/js/article.jsx',
+    component: './src/js/component.jsx',
+    list:      './src/js/list.jsx'
+  },
+  output: {
+    path: __dirname + '/dist/js',
+    filename: '[name]-' + version + '.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.jsx|.js$/, include: __dirname + '/src/js', loader: 'babel-loader'},
+      {test: /\.jsx|.js$/, include: __dirname + '/node_modules/react-tabs', loader: 'babel-loader'}
     ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
