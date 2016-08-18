f52b1f894cc567d874a417b502c4589762b4d2aa
Peter Siemens <peterjsiemens@gmail.com>
2016-08-18 15:37:23 -0700

    Clean dist folder before building static files

--- dispatch/static/gulpfile.js
+++ dispatch/static/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 
 var webpack = require('webpack-stream');
 var sass = require('gulp-sass');
+var clean = require('gulp-clean');
 
 gulp.task('sass', function () {
   return gulp.src('./src/styles/**/*.scss')
@@ -9,10 +10,15 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('webpack', function() {
+gulp.task('webpack', function () {
   return gulp.src('./src/js/**/*.jsx')
     .pipe(webpack( require('./webpack.config.js') ))
     .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('default', ['webpack', 'sass']);
+gulp.task('clean', function () {
+  return gulp.src('./dist/', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['clean', 'webpack', 'sass']);
