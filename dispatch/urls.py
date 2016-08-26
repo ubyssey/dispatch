cddf9437a326dc8d84e1b91dc46b0d47bc070e8e
Peter Siemens <peterjsiemens@gmail.com>
2016-08-26 12:47:30 -0700

    Add token authentication to API

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -9,6 +9,5 @@ from dispatch.apps.manager.urls import urlpatterns as admin_urls
 urlpatterns = [
     url(r'^admin/', include(admin_urls)),
     url(r'^api/', include(api_urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(ThemeHelper.get_theme_urls())),
 ]
