3aaae58fde3c0affbb25c6922fa7bcd2d95b24d1
Peter Siemens <peterjsiemens@gmail.com>
2016-08-29 14:02:55 -0700

    Create section actions

---
+++ dispatch/static/manager/src/js/actions/SectionActions.js
@@ -0,0 +1,25 @@
+import * as types from '../constants/ActionTypes'
+import DispatchAPI from '../api/dispatch'
+
+function requestSections() {
+  return {
+    type: types.SECTIONS_REQUEST
+  }
+}
+
+function receiveSections(sections) {
+  return {
+    type: types.SECTIONS_RECEIVE,
+    sections: sections
+  }
+}
+
+export function fetchSections() {
+  return function (dispatch) {
+    dispatch(requestSections())
+
+    return DispatchAPI.sections.fetchSections()
+      .then( response => response.json() )
+      .then( json => dispatch(receiveSections(json.results)) )
+  }
+}
