50bafe2d457ae6ce0bba7f77a8557aed5a98d847
Peter Siemens <peterjsiemens@gmail.com>
2016-08-29 14:02:27 -0700

    Separate clean functions in gulp file

--- dispatch/static/manager/gulpfile.js
+++ dispatch/static/manager/gulpfile.js
@@ -4,21 +4,26 @@ var webpack = require('webpack-stream');
 var sass = require('gulp-sass');
 var clean = require('gulp-clean');
 
-gulp.task('sass', ['clean'], function () {
+gulp.task('sass', ['clean-css'], function () {
   return gulp.src('./src/styles/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('webpack', ['clean'], function () {
+gulp.task('webpack', ['clean-js'], function () {
   return gulp.src('./src/js/**/*.jsx')
     .pipe(webpack( require('./webpack.config.js') ))
     .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('clean', function () {
-  return gulp.src('./dist/', {read: false})
+gulp.task('clean-css', function () {
+  return gulp.src('./dist/css/', {read: false})
     .pipe(clean());
 });
 
-gulp.task('default', ['clean', 'webpack', 'sass']);
+gulp.task('clean-js', function () {
+  return gulp.src('./dist/js/', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('default', ['webpack', 'sass']);
