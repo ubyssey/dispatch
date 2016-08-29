82a4eb4902999aeffa73addb022954c09a8218f3
Peter Siemens <peterjsiemens@gmail.com>
2016-08-29 14:03:12 -0700

    Separate auth and section reducers

--- dispatch/static/manager/src/js/reducers/AppReducer.js
+++ dispatch/static/manager/src/js/reducers/AppReducer.js
@@ -1,32 +1,9 @@
-import * as types from '../constants/ActionTypes'
-import Cookies from 'js-cookie'
+import { combineReducers } from 'redux'
 
-const initialState = {
-  token: Cookies.get('token'), // Get token stored in browser cookie
-  user: {},
-  sections: []
-}
+import authReducer from './AuthReducer'
+import sectionsReducer from './SectionsReducer'
 
-export default function appReducer(state = initialState, action) {
-  switch (action.type) {
-    case types.AUTH_REQUIRED:
-      return Object.assign({}, state, {
-        nextPath: action.nextPath
-      })
-    case types.AUTH_REQUEST_TOKEN:
-      return state
-    case types.AUTH_RECEIVE_TOKEN:
-      Cookies.set('token', action.token) // Persist token in browser cookie
-      return Object.assign({}, state, {
-        token: action.token,
-        user: {
-          email: action.email
-        },
-        nextPath: null
-      })
-    case types.AUTH_FAILURE_TOKEN:
-      return state
-    default:
-      return state
-  }
-}
+export default combineReducers({
+  auth: authReducer,
+  sections: sectionsReducer,
+})
