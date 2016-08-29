7d0d0cda4f9e4711f113ff73f774676a27fd62ce
Peter Siemens <peterjsiemens@gmail.com>
2016-08-29 14:04:09 -0700

    Create header component

--- dispatch/static/manager/src/js/components/Header.jsx
+++ dispatch/static/manager/src/js/components/Header.jsx
@@ -2,39 +2,58 @@ import React from 'react'
 import { Link } from 'react-router'
 
 export default function Header(props) {
+
+  const sections = props.sections.map( section => {
+    return (
+      <li className='c-header__link' key={section.id}>
+        <Link to={'/sections/' + section.slug}>{section.name}</Link>
+      </li>
+    )
+  })
+
   return (
     <header className='c-header'>
       <Link to='/' className='c-header__logo'>
         <i className='c-header__logo-icon fa fa-circle'></i>dispatch
       </Link>
-      <ul>
-        <li>News</li>
-        <li>Sports</li>
-        <li>Culture</li>
-        <li>Opinion</li>
-        <li>Features</li>
-        <li>Science</li>
-        <li>Blog</li>
-      </ul>
-      <ul className='right'>
-        <li className='dropdown'>
-          Manage<i className='fa fa-caret-down left'></i>
-          <ul>
-            <li><i className='fa fa-wrench right'></i>components</li>
-            <li><i className='fa fa-file-o right'></i>files</li>
-            <li><i className='fa fa-file-text-o right'></i>pages</li>
-            <li><i className='fa fa-list right'></i>sections</li>
-            <li><i className='fa fa-users right'></i>people</li>
-          </ul>
-        </li>
-        <li className='dropdown'>
-          <i className='fa fa-user right'></i> {props.user.email}
-          <ul>
-            <li>manage profile</li>
-            <li>logout</li>
-          </ul>
-        </li>
-      </ul>
+      <nav className='c-header__sections'>
+        <ul>{sections}</ul>
+      </nav>
+      <nav className='c-header__right'>
+        <ul>
+          <li className='c-header__dropdown'>
+            <a>Manage<i className='fa fa-caret-down left'></i></a>
+            <ul>
+              <li className='c-header__dropdown__link'>
+                <Link to='/components'><i className='fa fa-wrench right'></i>components</Link>
+              </li>
+              <li className='c-header__dropdown__link'>
+                <Link to='/files'><i className='fa fa-file-o right'></i>files</Link>
+              </li>
+              <li className='c-header__dropdown__link'>
+                <Link to='/pages'><i className='fa fa-file-text-o right'></i>pages</Link>
+              </li>
+              <li className='c-header__dropdown__link'>
+                <Link to='/sections'><i className='fa fa-list right'></i>sections</Link>
+              </li>
+              <li className='c-header__dropdown__link'>
+                <Link to='/people'><i className='fa fa-users right'></i>people</Link>
+              </li>
+            </ul>
+          </li>
+          <li className='c-header__dropdown'>
+            <a><i className='fa fa-user right'></i> {props.email}</a>
+            <ul>
+              <li className='c-header__dropdown__link'>
+                <Link to='/profile'>manage profile</Link>
+              </li>
+              <li className='c-header__dropdown__link'>
+                <Link to='/logout'>logout</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
