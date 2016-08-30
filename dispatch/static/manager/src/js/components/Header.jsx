884a494dcfae93bd4dd1a6040eedb577f23c1909
Peter Siemens <peterjsiemens@gmail.com>
2016-08-30 13:51:05 -0700

    Changing overall page styling to use flex box

--- dispatch/static/manager/src/js/components/Header.jsx
+++ dispatch/static/manager/src/js/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import LoadingBar from 'react-redux-loading-bar'
 
 export default function Header(props) {
 
@@ -13,47 +14,50 @@ export default function Header(props) {
 
   return (
     <header className='c-header'>
-      <Link to='/' className='c-header__logo'>
-        <i className='c-header__logo-icon fa fa-circle'></i>dispatch
-      </Link>
-      <nav className='c-header__sections'>
-        <ul>{sections}</ul>
-      </nav>
-      <nav className='c-header__right'>
-        <ul>
-          <li className='c-header__dropdown'>
-            <a>Manage<i className='fa fa-caret-down left'></i></a>
-            <ul>
-              <li className='c-header__dropdown__link'>
-                <Link to='/components'><i className='fa fa-wrench right'></i>components</Link>
-              </li>
-              <li className='c-header__dropdown__link'>
-                <Link to='/files'><i className='fa fa-file-o right'></i>files</Link>
-              </li>
-              <li className='c-header__dropdown__link'>
-                <Link to='/pages'><i className='fa fa-file-text-o right'></i>pages</Link>
-              </li>
-              <li className='c-header__dropdown__link'>
-                <Link to='/sections'><i className='fa fa-list right'></i>sections</Link>
-              </li>
-              <li className='c-header__dropdown__link'>
-                <Link to='/people'><i className='fa fa-users right'></i>people</Link>
-              </li>
-            </ul>
-          </li>
-          <li className='c-header__dropdown'>
-            <a><i className='fa fa-user right'></i> {props.email}</a>
-            <ul>
-              <li className='c-header__dropdown__link'>
-                <Link to='/profile'>manage profile</Link>
-              </li>
-              <li className='c-header__dropdown__link'>
-                <Link to='/logout'>logout</Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
+      <div className='c-header__inner'>
+        <Link to='/' className='c-header__logo'>
+          <i className='c-header__logo-icon fa fa-circle'></i>dispatch
+        </Link>
+        <nav className='c-header__sections'>
+          <ul>{sections}</ul>
+        </nav>
+        <nav className='c-header__right'>
+          <ul>
+            <li className='c-header__dropdown'>
+              <a>Manage<i className='fa fa-caret-down left'></i></a>
+              <ul>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/components'><i className='fa fa-wrench right'></i>components</Link>
+                </li>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/files'><i className='fa fa-file-o right'></i>files</Link>
+                </li>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/pages'><i className='fa fa-file-text-o right'></i>pages</Link>
+                </li>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/sections'><i className='fa fa-list right'></i>sections</Link>
+                </li>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/people'><i className='fa fa-users right'></i>people</Link>
+                </li>
+              </ul>
+            </li>
+            <li className='c-header__dropdown'>
+              <a><i className='fa fa-user right'></i> {props.email}</a>
+              <ul>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/profile'>manage profile</Link>
+                </li>
+                <li className='c-header__dropdown__link'>
+                  <Link to='/logout'>logout</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <LoadingBar />
     </header>
   )
 }
