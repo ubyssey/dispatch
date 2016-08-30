bb5301136313d00fb95fea7c363810218ab92688
Peter Siemens <peterjsiemens@gmail.com>
2016-08-30 13:02:07 -0700

    Add loading bar

--- dispatch/static/manager/src/js/actions/ArticlesActions.js
+++ dispatch/static/manager/src/js/actions/ArticlesActions.js
@@ -1,46 +1,16 @@
 import * as types from '../constants/ActionTypes'
 import DispatchAPI from '../api/dispatch'
 
-function requestArticles() {
-  return {
-    type: types.ARTICLES_REQUEST
-  }
-}
-
-function receiveArticles(articles) {
-  return {
-    type: types.ARTICLES_RECEIVE,
-    articles: articles
-  }
-}
-
-function requestArticle() {
-  return {
-    type: types.ARTICLE_REQUEST
-  }
-}
-
-function receiveArticle(article) {
-  return {
-    type: types.ARTICLE_RECEIVE,
-    article: article
-  }
-}
-
 export function fetchArticles(params) {
-  return function (dispatch) {
-    dispatch(requestArticles())
-
-    return DispatchAPI.articles.fetchArticles(params)
-      .then( json => dispatch(receiveArticles(json.results)) )
+  return {
+    type: types.FETCH_ARTICLES,
+    payload: DispatchAPI.articles.fetchArticles(params).then( json => json.results )
   }
 }
 
 export function fetchArticle(articleId) {
-  return function (dispatch) {
-    dispatch(requestArticle())
-
-    return DispatchAPI.articles.fetchArticle(articleId)
-      .then( json => dispatch(receiveArticle(json)) )
+  return {
+    type: types.FETCH_ARTICLE,
+    payload: DispatchAPI.articles.fetchArticle(articleId)
   }
 }
