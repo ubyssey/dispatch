5f702042079297a26098e0e4f9120e24793b5570
Peter Siemens <peterjsiemens@gmail.com>
2016-09-15 14:32:40 -0700

    Separate article and articles entities

--- dispatch/static/manager/src/js/reducers/EntitiesReducer.js
+++ dispatch/static/manager/src/js/reducers/EntitiesReducer.js
@@ -2,16 +2,31 @@ import * as types from '../constants/ActionTypes'
 
 const initialState = {
   articles: {},
+  article: {},
   sections: {}
 }
 
 export default function entitiesReducer(state = initialState, action) {
   switch (action.type) {
+
+    // Articles
     case types.FETCH_ARTICLES + '_FULFILLED':
-    case types.FETCH_ARTICLE  + '_FULFILLED':
       return Object.assign({}, state, {
         articles: Object.assign({}, state.articles, action.payload.entities.articles)
       })
+
+    // Article
+    case types.FETCH_ARTICLE + '_PENDING':
+      return Object.assign({}, state, {
+        article: {}
+      })
+    case types.FETCH_ARTICLE  + '_FULFILLED':
+      return Object.assign({}, state, {
+        articles: Object.assign({}, state.articles, action.payload.entities.articles),
+        article: action.payload.entities.articles
+      })
+
+    // Sections
     case types.FETCH_SECTIONS + '_FULFILLED':
       return Object.assign({}, state, {
         sections: Object.assign({}, state.sections, action.payload.entities.sections)
