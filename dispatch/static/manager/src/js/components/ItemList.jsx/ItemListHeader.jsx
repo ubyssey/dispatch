33d9229310124972d702a0afa74acd3b154914b8
Peter Siemens <peterjsiemens@gmail.com>
2016-09-29 12:40:56 -0700

    Only show pagination if items isn't empty

--- dispatch/static/manager/src/js/components/ItemList.jsx/ItemListHeader.jsx
+++ dispatch/static/manager/src/js/components/ItemList.jsx/ItemListHeader.jsx
@@ -37,8 +37,8 @@ export default function ItemListHeader(props) {
           <Button onClick={handleDeleteItems} disabled={!props.items.selected.length}>Delete</Button>
         </div>
         <div className='c-item-list__header__right'>
-        {props.items.isLoaded ? renderPagination() : null}
-        <ItemListSearchBar searchItems={props.actions.searchItems} />
+        {props.items.isLoaded && props.items.data.length ? renderPagination() : null}
+        <ItemListSearchBar query={props.location.query.q} searchItems={props.actions.searchItems} />
         </div>
       </div>
     </Toolbar>
