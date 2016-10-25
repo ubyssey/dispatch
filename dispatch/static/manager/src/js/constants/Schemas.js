c1c02f3e252e885f69da73a8b195aa7bd29127c0
Peter Siemens <peterjsiemens@gmail.com>
2016-10-25 14:57:09 -0700

    Add support for adding values to multi-select

--- dispatch/static/manager/src/js/constants/Schemas.js
+++ dispatch/static/manager/src/js/constants/Schemas.js
@@ -1,6 +1,12 @@
-import { Schema } from 'normalizr'
+import { Schema, arrayOf } from 'normalizr'
 
 export const articleSchema = new Schema('articles')
 export const sectionSchema = new Schema('sections')
-export const imageSchema = new Schema('images')
 export const personSchema = new Schema('persons')
+
+// Image Schema
+export const imageSchema = new Schema('images')
+
+imageSchema.define({
+  authors: arrayOf(personSchema)
+});
