bc11eee1a75689b8dd894a07cd2b0ae2218710eb
Peter Siemens <peterjsiemens@gmail.com>
2016-11-03 14:37:20 -0700

    Create new value if doesn't already exist

--- dispatch/static/manager/src/js/actions/ImagesActions.js
+++ dispatch/static/manager/src/js/actions/ImagesActions.js
@@ -1,9 +1,11 @@
+import R from 'ramda'
 import { normalize, arrayOf } from 'normalizr'
 import { push } from 'react-router-redux'
 
 import * as types from '../constants/ActionTypes'
 import { imageSchema } from '../constants/Schemas'
 import DispatchAPI from '../api/dispatch'
+import { createPersonHelper } from './PersonsActions'
 
 export function fetchImages(params) {
   return {
@@ -44,6 +46,31 @@ export function updateImage(token, imageId, data) {
     type: types.UPDATE_IMAGE,
     payload: DispatchAPI.images.updateImage(token, imageId, data)
       .then( json => normalize(json, imageSchema) )
+  }
+}
+
+export function addAuthorToImage(token, image, authorId) {
+  let newAuthors = R.append(authorId, image.authors)
+  return updateImage(token, image.id, { authors: newAuthors })
+}
+
+export function removeAuthorFromImage(token, image, authorId) {
+  let newAuthors = R.remove(
+    R.findIndex(R.equals(authorId), image.authors),
+    1,
+    image.authors
+  )
+
+  return updateImage(token, image.id, { authors: newAuthors })
+}
+
+export function createAndAddAuthorToImage(token, image, authorName) {
+  return function(dispatch) {
+    DispatchAPI.persons.createPerson(token, authorName)
+      .then( person => {
+        dispatch(createPersonHelper(person))
+        dispatch(addAuthorToImage(token, image, person.id))
+      })
   }
 }
 
--- dispatch/static/manager/src/js/components/inputs/MultiSelectInput.jsx
+++ dispatch/static/manager/src/js/components/inputs/MultiSelectInput.jsx
@@ -8,11 +8,17 @@ export default class MultiSelectInput extends React.Component {
 
   constructor(props) {
     super(props)
+
     this.removeValue = this.removeValue.bind(this)
     this.pageClick = this.pageClick.bind(this)
     this.handleMouseDown = this.handleMouseDown.bind(this)
     this.handleMouseUp = this.handleMouseUp.bind(this)
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+
+    this.renderSelected = this.renderSelected.bind(this)
+    this.renderResult = this.renderResult.bind(this)
+    this.isNotSelected = this.isNotSelected.bind(this)
 
     this.mouseIsDownOnField = false;
 
@@ -21,6 +27,7 @@ export default class MultiSelectInput extends React.Component {
       query: ''
     }
   }
+
   componentDidMount() {
     window.addEventListener('mousedown', this.pageClick, false)
     this.fetchResults()
@@ -44,7 +51,19 @@ export default class MultiSelectInput extends React.Component {
 
   handleInputChange(e) {
     e.preventDefault();
-    this.setState({ query: e.target.value }, this.fetchResults)
+    this.setState(
+      {
+        query: e.target.value,
+        isActive: true
+      },
+      this.fetchResults
+    )
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.createValue(this.state.query.trim())
+    }
   }
 
   fetchResults() {
@@ -52,66 +71,124 @@ export default class MultiSelectInput extends React.Component {
   }
 
   removeValue(id) {
-    let newValues = R.remove(
-      R.findIndex(R.equals(id), this.props.selected),
-      1,
-      this.props.selected
-    )
-
-    this.props.onUpdate(newValues)
+    this.props.removeValue(id)
   }
 
   addValue(id) {
-    let newValues = R.append(id, this.props.selected)
+    this.props.addValue(id)
+    this.closeDropdown()
+  }
 
-    this.props.onUpdate(newValues)
+  createValue(value) {
+    // Get entities in search results
+    const results = R.map(
+      id => this.props.entities[id],
+      this.props.results
+    )
+
+    // Get currently selected entities
+    const selected = R.map(
+      id => this.props.entities[id],
+      this.props.selected
+    )
+
+    // Determine if the value has already been created
+    let resultIndex = R.findIndex(
+      R.propEq(this.props.attribute, value),
+      results
+    )
+
+    let isResult = resultIndex !== -1
+
+    // Determine if value has already been added to selection
+    let isSelected = R.findIndex(
+      R.propEq(this.props.attribute, value),
+      selected
+    ) !== -1
+
+    if (isResult && !isSelected) {
+      // If value is in results and not selected, add to selection
+      this.props.addValue(results[resultIndex].id)
+      this.closeDropdown()
+    } else if (!isResult && !isSelected) {
+      // If value is not in results or selected, create and add to selection
+      this.props.createValue(value)
+      this.closeDropdown()
+    }
+
+  }
+
+  closeDropdown() {
+    this.setState({
+      isActive: false,
+      query: ''
+    })
+  }
+
+  renderSelected(id) {
+    const value = this.props.entities[id]
+    return (
+      <li
+        className='c-input--multi-select__value'
+        key={value.id}>
+        {value[this.props.attribute]}
+          <div
+            className='c-input--multi-select__value__icon'
+            onClick={() => this.removeValue(value.id)}>
+            <MdClose size={18} />
+          </div>
+        </li>
+    )
+  }
+
+  isNotSelected(id) {
+    return !R.contains(id, this.props.selected)
+  }
+
+  renderResult(id) {
+    const value = this.props.entities[id]
+    return (
+      <li
+        key={value.id}
+        className='c-input--multi-select__result'
+        onClick={() => this.addValue(value.id)}>{value[this.props.attribute]}</li>
+    )
+  }
+
+  renderNoResults() {
+    return (
+      <div className='c-input--multi-select__no-results'>
+        {`Press enter to create "${this.state.query.trim()}"`}
+      </div>
+    )
   }
 
   render() {
-    const selected = this.props.selected.map( id => {
-      const value = this.props.entities[id];
-      return (
-        <li
-          className='c-input--multi-select__value'
-          key={value.id}>
-          {value[this.props.attribute]}
-            <div
-              className='c-input--multi-select__value__icon'
-              onClick={() => this.removeValue(value.id)}>
-              <MdClose size={18} />
-            </div>
-          </li>
-      )
-    })
-
-    const results = this.props.results.map( id => {
-      const value = this.props.entities[id];
-      return (
-        <li
-          key={value.id}
-          className='c-input--multi-select__result'
-          onClick={() => this.addValue(value.id)}>{value[this.props.attribute]}</li>
-      )
-    })
+    const selected = R.map(this.renderSelected, this.props.selected)
+    const results = R.map(this.renderResult, R.filter(this.isNotSelected, this.props.results))
 
     const dropdownBaseClassName = 'c-input--multi-select__dropdown'
     const dropdownClassName = dropdownBaseClassName + (this.state.isActive ? ` ${dropdownBaseClassName}--active` : '')
 
     return (
       <div
-        className='c-input c-input--multi-select'
-        onMouseDown={this.handleMouseDown}
-        onMouseUp={this.handleMouseUp}>
+        className='c-input c-input--multi-select'>
         <ul className='c-input--multi-select__values'>
           {selected}
         </ul>
-        <div className={dropdownClassName}>
-          <TextInput
-            onChange={this.handleInputChange}
-            placeholder='Add new' />
-          <ul className='c-input--multi-select__results'>
-            {results}
-          </ul>
+        <div
+          onMouseDown={this.handleMouseDown}
+          onMouseUp={this.handleMouseUp}>
+          <div className={dropdownClassName}>
+            <TextInput
+              onChange={this.handleInputChange}
+              onKeyPress={this.handleKeyPress}
+              value={this.state.query}
+              placeholder='Add new' />
+            <ul className='c-input--multi-select__results'>
+              {results.length ? results : this.renderNoResults()}
+            </ul>
+          </div>
         </div>
       </div>
     )
