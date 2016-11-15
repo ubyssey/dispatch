1087bce24931888a3350f9b0d6140405138bd4c6
Peter Siemens <peterjsiemens@gmail.com>
2016-11-14 21:06:05 -0800

    Disable image manager insert button when no image selected

--- dispatch/static/manager/src/js/components/inputs/Button.jsx
+++ dispatch/static/manager/src/js/components/inputs/Button.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+function getClassName(disabled) {
+  return 'c-input c-input--button' + (disabled ? ' c-input--disabled' : '');
+}
+
 export default function Button(props) {
+
+  function handleOnClick(e) {
+    if (!props.disabled) {
+      return props.onClick(e);
+    } else {
+      e.preventDefault();
+    }
+  }
+
   return (
-    <a className='c-input c-input--button' onClick={props.onClick}>{props.children}</a>
+    <a
+      className={getClassName(props.disabled)}
+      onClick={handleOnClick}>{props.children}</a>
   )
 }
