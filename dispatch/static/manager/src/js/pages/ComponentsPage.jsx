63fab447fc6bd66480d7d2828c0f0c96becbaefe
JimFung <jfung92@gmail.com>
2017-01-22 14:44:12 -0800

    everything now links to a dummy page also added a space for the items in the drop down so its not right beside the icon

---
+++ dispatch/static/manager/src/js/pages/ComponentsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ComponentsPage(props) {
+  return (
+    <h1>This is the Components Page</h1>
+  )
+}
---
+++ dispatch/static/manager/src/js/pages/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ProfilePage(props) {
+  return (
+    <h1>This is the Profile Page</h1>
+  )
+}
