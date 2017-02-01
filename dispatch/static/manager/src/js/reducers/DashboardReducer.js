85f7e5abf42d974e273cb8e538241c4249cb8f77
JimFung <jfung92@gmail.com>
2017-02-01 14:28:20 -0800

    wip i need to work on cs416 now

---
+++ dispatch/static/manager/src/js/reducers/DashboardReducer.js
@@ -0,0 +1,24 @@
+import R from 'ramda';
+
+import * as types from '../constants/ActionTypes'
+
+// Dashboard actions
+// export const FETCH_ACTIONS = 'FETCH_ACTIONS'
+// export const FETCH_RECENT = 'FETCH_RECENT'
+
+let DashboardReducer = (state = {}, action) => {
+  switch(action.type) {
+    case `${types.FETCH_ACTIONS}_PENDING`:
+      return R.merge(state, {isLoading: true})
+    case `${types.FETCH_ACTIONS}_FULFILLED`:
+      return R.merge(state, {
+        isLoading: false,
+        isLoaded: true,
+        data: action.payload.results
+      })
+    default:
+      return state
+  }
+}
+
+export default DashboardReducer
