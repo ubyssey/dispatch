716073a030c4ea16033e1e851be4992385ff1b27
Peter Siemens <peterjsiemens@gmail.com>
2017-02-01 22:52:11 -0800

    Get rid of .jsx file extension

--- dispatch/static/manager/src/js/actions/ArticlesActions.js
+++ dispatch/static/manager/src/js/actions/ArticlesActions.js
@@ -5,7 +5,7 @@ import * as types from '../constants/ActionTypes'
 import { articleSchema } from '../constants/Schemas'
 import DispatchAPI from '../api/dispatch'
 
-import ContentStateHelper from '../components/ContentEditor.jsx/ContentStateHelper'
+import ContentStateHelper from '../components/ContentEditor/ContentStateHelper'
 
 export function fetchArticles(token, query) {
   return {
