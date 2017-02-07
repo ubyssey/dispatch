be9304c45e9731b55bd68f8faeca840313b4a60d
Peter Siemens <peterjsiemens@gmail.com>
2017-02-06 20:37:27 -0800

    Add tests for Integrations

---
+++ tests/test_core.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+
+from dispatch.apps.core.integrations import BaseIntegration
+from dispatch.apps.core.models import IntegrationSetting
+
+class IntegrationTestCase(TestCase):
+
+    class TestIntegration(BaseIntegration):
+
+        ID = 'test-integration'
+
+    def test_integration_returns_empty_settings(self):
+
+        self.assertEqual(self.TestIntegration.get_settings(), {})
+
+    def test_integration_adds_new_settings(self):
+
+        self.TestIntegration.update_setting('setting_a', 'a')
+        self.TestIntegration.update_setting('setting_b', 'b')
+
+        settings = self.TestIntegration.get_settings()
+
+        self.assertEqual(settings['setting_a'], 'a')
+        self.assertEqual(settings['setting_b'], 'b')
+
+    def test_integration_updates_existing_setting(self):
+
+        # Set setting_a to 1
+        self.TestIntegration.update_setting('setting_a', '1')
+
+        # Verify that setting_a has been saved as 1
+        settings = self.TestIntegration.get_settings()
+        self.assertEqual(settings['setting_a'], '1')
+
+        # Update setting_a to 2
+        self.TestIntegration.update_setting('setting_a', '2')
+
+        # Verify that setting_a has been updated to 2
+        settings = self.TestIntegration.get_settings()
+        self.assertEqual(settings['setting_a'], '2')
