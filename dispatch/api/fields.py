826e8b405f87b1d2f42ebfb4a6a6c995217d151a
Peter Siemens <peterjsiemens@gmail.com>
2017-02-09 12:07:24 -0800

    Add spaces

--- dispatch/api/fields.py
+++ dispatch/api/fields.py
@@ -3,7 +3,9 @@ import json
 from rest_framework import serializers
 
 class JSONField(serializers.Field):
+
     def to_internal_value(self, value):
         return value
+
     def to_representation(self, value):
         return value
