9788c16acb95437e95fe48b48f7aa57297b80a51
Peter Siemens <peterjsiemens@gmail.com>
2017-02-10 18:37:28 -0800

    Explicit return in SidebarNav

--- dispatch/static/manager/src/js/components/SidebarNav.js
+++ dispatch/static/manager/src/js/components/SidebarNav.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router'
 
 export default function SidebarNav(props) {
 
-  let items = props.items.map((item, i) => {
-    return (
-      <li className='c-sidebar-nav__link' key={i}>
-        <Link to={item.path}>{item.label}</Link>
-      </li>
-    )
-  })
+  let items = props.items.map((item, i) => (
+    <li className='c-sidebar-nav__link' key={i}>
+      <Link to={item.path}>{item.label}</Link>
+    </li>
+  ))
 
   return (
     <nav className='c-sidebar-nav'>
