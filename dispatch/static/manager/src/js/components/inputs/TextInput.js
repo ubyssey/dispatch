3c4d72c1888edeb201b2a70a9d0dbdff165e9f21
Peter Siemens <peterjsiemens@gmail.com>
2017-02-10 22:32:31 -0800

    Change client_secret to password field

--- dispatch/static/manager/src/js/components/inputs/TextInput.js
+++ dispatch/static/manager/src/js/components/inputs/TextInput.js
@@ -38,7 +38,7 @@ export default class TextInput extends React.Component {
       <input
         ref='input'
         className={`pt-input${ this.props.fill ? ' pt-fill' : '' }${ this.props.className ? ' ' + this.props.className : ''}`}
-        type='text'
+        type={this.props.type || 'text'}
         value={this.props.value}
         disabled={this.props.disabled}
         placeholder={this.props.placeholder}
