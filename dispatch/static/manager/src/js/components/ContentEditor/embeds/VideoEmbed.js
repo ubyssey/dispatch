1ed95970f0cfbe1c79e9eb79a64bb7c80f27eb03
JamieRL <lee.jamr@gmail.com>
2017-02-15 10:27:57 -0800

    added video embe file and linked to articlecontenteditor

---
+++ dispatch/static/manager/src/js/components/ContentEditor/embeds/VideoEmbed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { FormInput, TextInput } from '../../inputs'
+
+
+class VideoEmbedComponent extends React.Component {
+
+
+}
+
+export default {
+  type: 'Video',
+  component: VideoEmbedComponent
+}
