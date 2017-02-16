2af4dd433a74b4ea9f25a05697c1c7882a52fc81
JimFung <jfung92@gmail.com>
2017-02-16 00:16:46 -0800

    login is working again

--- dispatch/static/manager/src/js/api/dispatch.js
+++ dispatch/static/manager/src/js/api/dispatch.js
@@ -120,7 +120,7 @@ var DispatchAPI = {
       return postRequest('auth/token', null, payload)
     },
     deleteToken: (token) => {
-      return delRequest('/auth/token', null, token, token)
+      return delRequest('auth/token', null, token, token)
     }
   },
   sections: {
