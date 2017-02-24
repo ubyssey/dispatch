a7135ac8ab180a63e453ad762d7946fddd60fbeb
Peter Siemens <peterjsiemens@gmail.com>
2017-02-24 15:14:25 -0800

    Use single quotes

--- dispatch/static/manager/src/js/pages/DashboardPage.js
+++ dispatch/static/manager/src/js/pages/DashboardPage.js
@@ -19,15 +19,15 @@ class DashboardPageComponent extends React.Component {
   renderActivity() {
     return this.props.actions.data.map(
       (elem, i) => (
-        <li key={i} className="c-dashboard__activity__item">
+        <li key={i} className='c-dashboard__activity__item'>
           <div className='c-dashboard__activity__item__icon'>
-            <span className={`pt-icon-standard pt-icon-${elem.icon}`} aria-hidden="true" />
+            <span className={`pt-icon-standard pt-icon-${elem.icon}`} aria-hidden='true' />
           </div>
           <div className='c-dashboard__activity__item__text'>
             <span className='c-dashboard__activity__item__text__person'>{elem.meta.author}</span>
             {elem.meta.count == 1 ? ` ${elem.meta.action} ` : ` made ${elem.meta.count} ${elem.meta.action} to ` }
             <Link to={`articles/${elem.meta.id}`} dangerouslySetInnerHTML={{__html: elem.meta.headline}}/>
-            <span className="c-dashboard__leftlistspan">{` ${moment(elem.timestamp).from(moment())}`}</span>
+            <span className='c-dashboard__leftlistspan'>{` ${moment(elem.timestamp).from(moment())}`}</span>
           </div>
         </li>
       )
@@ -37,7 +37,7 @@ class DashboardPageComponent extends React.Component {
   renderRecent() {
     return this.props.recent.data.map(
       (elem, i) => (
-        <li key={i} className="c-dashboard_recent-articles__item">
+        <li key={i} className='c-dashboard_recent-articles__item'>
           <Link to={`/articles/${elem.id}`} dangerouslySetInnerHTML={{__html: elem.headline}} />
         </li>
       )
@@ -48,19 +48,19 @@ class DashboardPageComponent extends React.Component {
 
     return (
       <DocumentTitle title='Dispatch'>
-        <div className="c-dashboard">
-          <div className="c-dashboard__activity">
-            <h2 className="c-dashboard__header">Latest Activity</h2>
+        <div className='c-dashboard'>
+          <div className='c-dashboard__activity'>
+            <h2 className='c-dashboard__header'>Latest Activity</h2>
             <ul>{this.props.actions.isLoaded ? this.renderActivity() : null}</ul>
           </div>
-          <div className="c-dashboard__sidebar">
-            <h2 className="c-dashboard__header">Quick Actions</h2>
-            <ul className="c-dashboard_quick-actions">
-              <li className="c-dashboard_quick-actions__item"><LinkButton to={`/articles/new`}>New Article</LinkButton></li>
-              <li className="c-dashboard_quick-actions__item"><LinkButton to={``}>New Page</LinkButton></li>
+          <div className='c-dashboard__sidebar'>
+            <h2 className='c-dashboard__header'>Quick Actions</h2>
+            <ul className='c-dashboard_quick-actions'>
+              <li className='c-dashboard_quick-actions__item'><LinkButton to={`/articles/new`}>New Article</LinkButton></li>
+              <li className='c-dashboard_quick-actions__item'><LinkButton to={``}>New Page</LinkButton></li>
             </ul>
-            <div className="c-dashboard_recent-articles">
-              <h2 className="c-dashboard__header c-dashboard_recent-articles__header">Recent Articles</h2>
+            <div className='c-dashboard_recent-articles'>
+              <h2 className='c-dashboard__header c-dashboard_recent-articles__header'>Recent Articles</h2>
               <ul>{this.props.recent.isLoaded ? this.renderRecent() : null}</ul>
             </div>
           </div>
