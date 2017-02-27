c818e7cbb0720e2f2a608f56def61d921ee2dd8b
Peter Siemens <peterjsiemens@gmail.com>
2017-02-26 23:17:23 -0800

    Remove manager app views from Dispatch

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import include, url
+from django.shortcuts import render_to_response
 
 from rest_framework import routers
 
 from dispatch.helpers.theme import ThemeHelper
 from dispatch.apps.api.urls import urlpatterns as api_urls
-from dispatch.apps.manager.urls import urlpatterns as admin_urls
+
+def admin(request):
+    """Render HTML entry point for manager app."""
+    return render_to_response('manager/index.html', {})
 
 urlpatterns = [
-    url(r'^admin/?.*/', include(admin_urls)),
+    url(r'^admin/?.*/', admin),
     url(r'^api/', include(api_urls)),
     url(r'^', include(ThemeHelper.get_theme_urls())),
 ]
