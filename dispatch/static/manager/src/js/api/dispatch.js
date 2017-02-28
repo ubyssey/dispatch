12db51415cabb1b3e3a6b690d03262fa20ca85dd
JimFung <jfung92@gmail.com>
2017-02-28 07:09:51 -0800

    not passing token into api call for logout

--- dispatch/static/manager/src/js/api/dispatch.js
+++ dispatch/static/manager/src/js/api/dispatch.js
@@ -133,7 +133,7 @@ var DispatchAPI = {
       return postRequest('auth/token', null, payload)
     },
     deleteToken: (token) => {
-      return delRequest('auth/token', null, token, token)
+      return delRequest('auth/token', null, null, token)
     }
   },
   sections: {
