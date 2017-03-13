b3a8b6d04642358e161c0def8ad1b147f85e4257
Peter Siemens <peterjsiemens@gmail.com>
2017-03-12 23:57:20 -0700

    Run tests from dispatch-admin script

--- dispatch/tests/cases.py
+++ dispatch/tests/cases.py
@@ -5,16 +5,25 @@ from rest_framework.authtoken.models import Token
 
 from dispatch.apps.core.models import User
 
+TEST_EMAIL = 'test@test.com'
+TEST_PASSWORD = 'testing123'
+
 class DispatchAPITestCase(TestCase):
     @classmethod
     def setUpClass(cls):
         super(DispatchAPITestCase, cls).setUpClass()
-        user = User.objects.create_user('test@test.com', 'testing123')
+
+        # Create dummy user for testing
+        user = User.objects.create_user(TEST_EMAIL, TEST_PASSWORD)
         (token, created) = Token.objects.get_or_create(user=user)
 
     def setUp(self):
-        user = User.objects.get(email='test@test.com')
+        # Get user token
+        user = User.objects.get(email=TEST_EMAIL)
         token = Token.objects.get(user_id=user.id)
 
+        # Create new API Client instance
         self.client = APIClient()
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        # Set user auth token header
+        self.client.credentials(HTTP_AUTHORIZATION='Token %s' % token.key)
--- dispatch/tests/test_api_articles.py
+++ dispatch/tests/test_api_articles.py
@@ -2,11 +2,36 @@ from django.core.urlresolvers import reverse
 
 from rest_framework import status
 
-from .cases import DispatchAPITestCase
+from dispatch.tests.cases import DispatchAPITestCase
 
 from dispatch.apps.content.models import Article, Person, Section
 
 class ArticlesTests(DispatchAPITestCase):
+
+    def _create_article(self):
+        """
+        Create a dummy article instance
+        """
+
+        # Create test person
+        person = Person.objects.create(full_name='Test Person')
+        person.save()
+
+        # Create test section
+        section = Section.objects.create(name='Test Section', slug='test')
+        section.save()
+
+        url = reverse('api-articles-list')
+
+        data = {
+            'headline': 'Test headline',
+            'section_id': section.id,
+            'author_ids': [person.id],
+            'slug': 'test-article',
+            'content_json': '[]'
+        }
+
+        return self.client.post(url, data, format='json')
 
     def test_create_article_unauthorized(self):
         """
@@ -53,31 +78,6 @@ class ArticlesTests(DispatchAPITestCase):
         self.assertTrue('content_json' in response.data)
         self.assertTrue('slug' in response.data)
         self.assertTrue('section_id' in response.data)
-
-    def _create_article(self):
-        """
-        Create a dummy article instance
-        """
-
-        # Create test person
-        person = Person.objects.create(full_name='Test Person')
-        person.save()
-
-        # Create test section
-        section = Section.objects.create(name='Test Section', slug='test')
-        section.save()
-
-        url = reverse('api-articles-list')
-
-        data = {
-            'headline': 'Test headline',
-            'section_id': section.id,
-            'author_ids': [person.id],
-            'slug': 'test-article',
-            'content_json': '[]'
-        }
-
-        return self.client.post(url, data, format='json')
 
     def test_create_article(self):
         """
