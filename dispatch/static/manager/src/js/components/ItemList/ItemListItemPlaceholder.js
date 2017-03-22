a23d394142df075d40eff4a6f74a387383042860
Peter Siemens <peterjsiemens@gmail.com>
2017-03-22 14:40:50 -0700

    Clean up item list JSX

--- dispatch/static/manager/src/js/components/ItemList/ItemListItemPlaceholder.js
+++ dispatch/static/manager/src/js/components/ItemList/ItemListItemPlaceholder.js
@@ -6,7 +6,10 @@ export default function ItemListItemPlaceholder() {
   return (
     <li className='c-item-list__item'>
       <div className='c-item-list__item__cell c-item-list__item__cell--checkbox'>
-        <input type='checkbox' disabled={true} checked={false} />
+        <input
+          type='checkbox'
+          disabled={true} 
+          checked={false} />
       </div>
       <div className='c-item-list__item__cell'>
         <PlaceholderBar />
