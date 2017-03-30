d406ac0e250e560f39ba8bccab7991ceb5b33d84
Lo <bennyloj@ead.ubc.ca>
2017-03-30 13:30:03 -0700

    Test create incomplete section passes

--- dispatch/tests/test_section.py
+++ dispatch/tests/test_section.py
@@ -25,10 +25,15 @@ class SectionsTests(DispatchAPITestCase):
 		topic='test',
 		tags='test')
 		article.save()
+		
+		data = {
+			'name': 'Test name',
+			'slug': 'test-section',
+		}
 
 		url = reverse('api-sections-list')
 
-		return self.client.post(url, None, format='json')
+		return self.client.post(url, data, format='json')
 
 	def test_create_section_unauthorized(self):
 		"""
@@ -54,3 +59,21 @@ class SectionsTests(DispatchAPITestCase):
 		response = self.client.post(url, None, format='json')
 
 		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+		
+	def test_create_incomplete_section(self):
+		"""
+		Create section should fail with missing required fields
+		"""
+		
+		url = reverse('api-sections-list')
+		
+		# Section data is missing slug
+		data = {
+			'name': 'test',
+		}
+		
+		response = self.client.post(url, data, format='json')
+		
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+		
+		self.assertTrue('slug' in response.data)
