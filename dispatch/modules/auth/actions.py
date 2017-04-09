e5ebaf6e9515c77943763f75bb27a4526ed79b64
Peter Siemens <peterjsiemens@gmail.com>
2017-04-08 17:13:47 -0700

    Associate actions with User instead of Person

--- dispatch/modules/auth/actions.py
+++ dispatch/modules/auth/actions.py
@@ -43,15 +43,16 @@ def list_actions(count=25):
 
         if n < len(actions) - 1:
             next_action = actions[n+1]
-            if action.person == next_action.person and action.object_type == next_action.object_type and action.object_id == next_action.object_id and action.action == next_action.action:
+            if action.user_id == next_action.user_id and action.object_type == next_action.object_type and action.object_id == next_action.object_id and action.action == next_action.action:
                 continue
 
         if action.object_type == 'article':
             try:
                 article = Article.objects.get(parent_id=action.object_id, head=True)
+
                 meta = {
                     'id': action.object_id,
-                    'author': action.person.full_name,
+                    'author': action.user.person.full_name,
                     'headline': article.headline,
                     'count': count,
                     'action': SINGULAR[action.action] if count == 1 else PLURAL[action.action],
@@ -69,11 +70,11 @@ def list_actions(count=25):
 
     return results
 
-def recent_articles(person, count=5):
+def recent_articles(user, count=5):
 
     time = datetime.datetime.now() - datetime.timedelta(days=7)
 
-    actions = Action.objects.filter(person=person, object_type='article', timestamp__gt=time).order_by('-timestamp')
+    actions = Action.objects.filter(user=user, object_type='article', timestamp__gt=time).order_by('-timestamp')
     articles = []
 
     for action in actions:
