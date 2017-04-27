6af3bb0d889859793eca87b4a317b4deb2074acf
Peter Siemens <peterjsiemens@gmail.com>
2017-04-27 10:48:20 -0700

    Formatting for toolbar

--- dispatch/static/manager/src/js/components/SectionEditor/SectionToolbar.js
+++ dispatch/static/manager/src/js/components/SectionEditor/SectionToolbar.js
@@ -2,21 +2,35 @@ import React from 'react'
 
 import { AnchorButton, Intent } from '@blueprintjs/core'
 
-import Toolbar from '../Toolbar'
+import { Toolbar, ToolbarLeft, ToolbarRight, ToolbarTitle } from '../Toolbar'
 
 export default function SectionToolbar(props) {
 
+  const editTitle = (
+    <ToolbarTitle><span className='u-text-light'>Section: </span><span>{props.name}</span></ToolbarTitle>
+  )
+
+  const newTitle = (
+    <ToolbarTitle><span className='u-text-light'>New section</span></ToolbarTitle>
+  )
+
   return (
     <Toolbar>
-      <AnchorButton
-        intent={Intent.SUCCESS}
-        onClick={() => props.saveSection()}>{props.isNew ? 'Save' : 'Update'}</AnchorButton>
-      <AnchorButton
-        intent={Intent.DANGER}
-        disabled={props.isNew}
-        onClick={() => props.deleteSection()}>
+      <ToolbarLeft>
+        <AnchorButton>Back</AnchorButton>
+        {props.isNew ? newTitle : editTitle}
+      </ToolbarLeft>
+      <ToolbarRight>
+        <AnchorButton
+          intent={Intent.SUCCESS}
+          onClick={() => props.saveSection()}>{props.isNew ? 'Save' : 'Update'}</AnchorButton>
+        <AnchorButton
+          intent={Intent.DANGER}
+          disabled={props.isNew}
+          onClick={() => props.deleteSection()}>
           <span className='pt-icon-standard pt-icon-trash'></span>Delete
         </AnchorButton>
+      </ToolbarRight>
     </Toolbar>
   )
 }
