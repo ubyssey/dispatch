5ea34507003897b5919f0363bb3d2bfeda0ded45
Peter Siemens <peterjsiemens@gmail.com>
2017-04-29 23:20:28 -0700

    Create generic reducer class

--- dispatch/static/manager/src/js/util/helpers.js
+++ dispatch/static/manager/src/js/util/helpers.js
@@ -3,3 +3,7 @@ import moment from 'moment'
 export function humanizeDatetime(timestamp) {
   return moment(timestamp).format('lll') // April 18, 2017 12:50 PM
 }
+
+export function pending(actionType) { return `${actionType}_PENDING` }
+export function fulfilled(actionType) { return `${actionType}_FULFILLED` }
+export function rejected(actionType) { return `${actionType}_REJECTED` }
