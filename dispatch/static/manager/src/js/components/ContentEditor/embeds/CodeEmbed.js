d488e11fcb4e23e78f5ba7ef28a87b23b6dd5e98
JamieRL <lee.jamr@gmail.com>
2017-05-03 22:40:12 -0700

    add button for code embed to toolbar. Padding for insert button on video embeds

---
+++ dispatch/static/manager/src/js/components/ContentEditor/embeds/CodeEmbed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class CodeEmbedComponent extends React.Component {
+
+}
+
+export default {
+  type: 'code',
+  component: CodeEmbedComponent,
+  defaultData: {
+    body: null
+  }
+}
