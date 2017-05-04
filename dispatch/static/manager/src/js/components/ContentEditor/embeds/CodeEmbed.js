bc63aadfed60b55b1699bed66e4d7c8317df21d9
JamieRL <lee.jamr@gmail.com>
2017-05-04 00:11:33 -0700

    added ace editor

--- dispatch/static/manager/src/js/components/ContentEditor/embeds/CodeEmbed.js
+++ dispatch/static/manager/src/js/components/ContentEditor/embeds/CodeEmbed.js
@@ -1,8 +1,49 @@
 import React from 'react'
 
+import AceEditor from 'react-ace'
+import { Button } from '@blueprintjs/core'
+
+import 'brace/mode/html'
+import 'brace/theme/chrome'
+
+
+require('../../../../styles/components/embeds/code.scss')
 
 class CodeEmbedComponent extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      embedValue: ''
+    }
+  }
+  onCodeChange(newValue){
+    this.setState({
+      embedValue: newValue
+    })
+  }
+
+  render(){
+    return (
+      <div className='o-embed--code'>
+        <AceEditor
+          mode='html'
+          theme='chrome'
+          width='100%'
+          height='300px'
+          value={this.state.embedValue}
+          defaultValue='<h1>Enter html code here</h1>'
+          showPrintMargin={false}
+          onChange={(value) => this.onCodeChange(value)}
+          className='o-embed--code_editor'
+          editorProps={{$blockScrolling: true}}
+        />
+        <div className='o-embed--code_editor_button_container'>
+          <Button>Insert</Button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default {
