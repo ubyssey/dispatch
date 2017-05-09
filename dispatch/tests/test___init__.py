9e7641886c2e5a0e66918e937076bf801ea622d2
Axel-Jacobsen <axelnj44@gmail.com>
2017-05-09 11:17:27 -0700

    fixes

--- dispatch/tests/test___init__.py
+++ dispatch/tests/test___init__.py
@@ -1,8 +1,8 @@
-import dispatch.__init__
+import dispatch
 from django.test import TestCase
 
 class __init__Test(TestCase):
 
     def test_get_version(self):
 
-        return dispatch.__init__.__version__
+        return dispatch.__version__
