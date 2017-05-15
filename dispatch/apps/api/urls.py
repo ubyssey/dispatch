ffa308f9aa23f47d772c52181d695128eb8eeb13
Peter Siemens <peterjsiemens@gmail.com>
2017-05-15 13:56:41 -0700

    Remove legacy code for pages and components

--- dispatch/apps/api/urls.py
+++ dispatch/apps/api/urls.py
@@ -20,17 +20,10 @@ router.register(r'dashboard', views.DashboardViewSet, base_name='api-dashboard')
 router.register(r'integrations', views.IntegrationViewSet, base_name='api-integrations')
 router.register(r'files',views.FileViewSet, base_name='api-files')
 
-component = views.ComponentViewSet.as_view({
-    'get': 'detail',
-    'post': 'update',
-})
-
 dashboard_recent_articles = views.DashboardViewSet.as_view({ 'get': 'list_recent_articles'})
 dashboard_user_actions = views.DashboardViewSet.as_view({ 'get': 'list_actions'})
 
 urlpatterns = format_suffix_patterns([
-    # Components route
-    url(r'^components/(?P<slug>[\w-]+)/$', component, name='component'),
     # Dashboard routes
     url(r'^dashboard/recent', dashboard_recent_articles, name='dashboard_recent_articles'),
     url(r'^dashboard/actions', dashboard_user_actions, name='dashboard_user_actions'),
