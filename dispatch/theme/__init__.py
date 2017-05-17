88bdf5fb2f3beb0d66a57276ec50ba7bdf181d19
Axel-Jacobsen <axelnj44@gmail.com>
2017-05-17 16:07:12 -0700

    data isn't beind deleted between test_widget_render.py tests, not sure how to fix

--- dispatch/theme/__init__.py
+++ dispatch/theme/__init__.py
@@ -1,10 +1,13 @@
-from dispatch.theme.widgets import Widget, Zone, validate_widget, validate_zone
+from collections import OrderedDict
 
-class Registry(object):
+from dispatch.theme.validators import validate_widget, validate_zone
+from dispatch.theme.exceptions import ZoneNotFound, WidgetNotFound
+
+class ThemeRegistry(object):
 
     def __init__(self):
-        self.zones = {}
-        self.widgets = {}
+        self.zones = OrderedDict()
+        self.widgets = OrderedDict()
 
     def zone(self, zone):
         validate_zone(zone)
@@ -14,6 +17,42 @@ class Registry(object):
     def widget(self, widget):
         validate_widget(widget)
         self.widgets[widget.id] = widget
+
+        for zone in widget.zones:
+            try:
+                zone.register_widget(widget)
+            except KeyError:
+                raise InvalidWidget(
+                    "Zone with id '%s' must be registered before it can be used by %s" % (zone.id, widget.name)
+                )
+
         return widget
 
-register = Registry()
+register = ThemeRegistry()
+
+class ThemeManager(object):
+
+    class Zones:
+
+        @staticmethod
+        def list():
+            """Return list of registered zones"""
+            return [Z() for Z in register.zones.values()]
+
+        @staticmethod
+        def get(id):
+            """Return a specific zone"""
+            try:
+                return register.zones[id]()
+            except KeyError:
+                raise ZoneNotFound("Zone with id '%s' does not exist" % id)
+
+    class Widgets:
+
+        @staticmethod
+        def get(id):
+            """Return a specific widget"""
+            try:
+                return register.widgets[id]()
+            except KeyError:
+                raise WidgetNotFound("Widget with id '%s' does not exist" % id)
--- dispatch/theme/fields.py
+++ dispatch/theme/fields.py
@@ -7,7 +7,7 @@ class InvalidField(Exception):
 class Field(object):
     """Base class for all widget fields"""
 
-    def __init__(self, label, many=True):
+    def __init__(self, label, many=False):
         self.label = label
         self.many = many
 
@@ -24,7 +24,10 @@ class Field(object):
 
     def to_json(self):
         """Returns JSON representation of field data"""
-        return self.data
+        return {
+            'label' : self.label,
+            'data' : self.data
+        }
 
     def prepare_data(self):
         """Prepares field data for use in a template"""
@@ -74,16 +77,20 @@ class ArticleField(Field):
         return serializer.data
 
     def to_json(self):
-        if self.many:
-            return {
-                'label' : self.label,
-                'data' : map(self.get_article_json, self.data)
-            }
-        else:
-            return {
-                'label' : self.label,
-                'data' : self.get_article_json(self.data)
-            }
+
+        def get_data():
+            if not self.data:
+                return
+
+            if self.many:
+                return map(self.get_article_json, self.data)
+            else:
+                return self.get_article_json(self.data)
+
+        return {
+            'label': self.label,
+            'data': get_data()
+        }
 
     def prepare_data(self):
         if self.many:
@@ -97,15 +104,11 @@ class ImageField(Field):
 
     def validate(self):
         if self.many:
-            return {
-                'label' : self.label,
-                'data' : map(self.get_image_json, self.data)
-            }
+            if not all( [isinstance(id, int) for id in self.data] ):
+                raise InvalidField('Data must be list of integers')
         else:
-            return {
-                'label' : self.label,
-                'data' : self.get_image_json(self.data)
-            }
+            if not isinstance(self.data, int):
+                raise InvalidField('Data must be an integer')
 
     def get_image(self, id):
         try:
@@ -119,16 +122,20 @@ class ImageField(Field):
         return serializer.data
 
     def to_json(self):
-        if self.many:
-            return {
-                'label' : self.label,
-                'data' : map(self.get_image_json, self.data)
-            }
-        else:
-            return {
-                'label' : self.label,
-                'data' : self.get_image_json(self.data)
-            }
+
+        def get_data():
+            if not self.data:
+                return
+
+            if self.many:
+                return map(self.get_image_json, self.data)
+            else:
+                return self.get_image_json(self.data)
+
+        return {
+            'label': self.label,
+            'data': get_data()
+        }
 
     def prepare_data(self):
         if self.many:
