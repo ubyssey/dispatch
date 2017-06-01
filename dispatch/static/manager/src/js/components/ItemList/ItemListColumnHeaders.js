d9d879493a63de0fd179768a36ff21d2d3e50cc6
Jonathan Chapple <joncpl@gmail.com>
2017-06-01 13:18:48 -0700

    implement itemlist column headers

---
+++ dispatch/static/manager/src/js/components/ItemList/ItemListColumnHeaders.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import R from 'ramda'
+
+export default function ItemListColumnHeaders(props) {
+  if (!props.headers) {
+    return null
+  }
+
+  // blank first column for checkbox
+  const headers = R.insert(0, '', props.headers)
+
+  const items = headers.map((elem, i) => {
+    return (
+      <div key={i} className='c-item-list__item__cell'>
+        {elem}
+      </div>
+    )
+  })
+
+  return (
+    <li className='c-item-list__item c-item-list__column__header'>
+      {items}
+    </li>
+  )
+}
