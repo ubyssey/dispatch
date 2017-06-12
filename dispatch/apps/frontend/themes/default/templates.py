aef7dd554c0ba582587401ab64ff9958b1d322fd
Axel-Jacobsen <axelnj44@gmail.com>
2017-06-12 15:38:54 -0700

    More files that can be deleted

--- dispatch/apps/frontend/themes/default/templates.py
+++
@@ -1,9 +0,0 @@
-from dispatch.apps.frontend.helpers import templates
-from dispatch.apps.frontend.templates import BaseTemplate
-
-class Default(BaseTemplate):
-
-    NAME = 'Default'
-    SLUG = 'default'
-
-templates.register(Default)
