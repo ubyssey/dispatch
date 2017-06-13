77e38f2fcbdbadaab3627d529fcf8d86f756de7e
Axel-Jacobsen <axelnj44@gmail.com>
2017-06-13 16:08:55 -0700

    Now using PhoneNumberField to validate the 'submitter_phone' field in the Event Form

--- dispatch/tests/helpers.py
+++ dispatch/tests/helpers.py
@@ -194,7 +194,7 @@ class DispatchTestHelpers(object):
         return client.post(url, data, format='json')
 
     @classmethod
-    def create_event(cls, client, title='Test event', description='Test description', host='test host', image=None, start_time='2017-05-25T12:00', end_time='2017-05-25T12:01', location='test location', address='123 UBC', category='academic', facebook_url='https://www.facebook.com/events/280150289084959', facebook_image_url='some other similar fb url', is_submission=False, is_published=False):
+    def create_event(cls, client, title='Test event', description='Test description', host='test host', image=None, start_time='2017-05-25T12:00', end_time='2017-05-25T12:01', location='test location', address='123 UBC', category='academic', facebook_url='https://www.facebook.com/events/280150289084959', facebook_image_url='some other similar fb url', is_submission=False, is_published=False, submitter_phone='+1 778 555 5555', submitter_email='developers@ubyssey.ca'):
 
         obj = DispatchMediaTestMixin()
 
@@ -212,7 +212,9 @@ class DispatchTestHelpers(object):
                 'facebook_url': facebook_url,
                 'facebook_image_url': facebook_image_url,
                 'is_submission': is_submission,
-                'is_published': is_published
+                'is_published': is_published,
+                'submitter_phone': submitter_phone,
+                'submitter_email': submitter_email
             }
 
             url = reverse('api-event-list')
