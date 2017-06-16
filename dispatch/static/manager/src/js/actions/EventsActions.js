ef90904e7a69a1bb43ac280b298cf6c075cfe724
Jonathan Chapple <joncpl@gmail.com>
2017-06-16 10:24:22 -0700

    remove custom approve action
    instead use improve prop checking logic

--- dispatch/static/manager/src/js/actions/EventsActions.js
+++ dispatch/static/manager/src/js/actions/EventsActions.js
@@ -1,5 +1,4 @@
 import { push } from 'react-router-redux'
-import { normalize } from 'normalizr'
 
 import * as types from '../constants/ActionTypes'
 import { eventSchema } from '../constants/Schemas'
@@ -19,26 +18,6 @@ class EventsActions extends ResourceActions {
 
     return dispatch => {
       dispatch(push({ pathname: '/events/', query: queryObj }))
-    }
-  }
-
-  approve(token, id, queryAfter) {
-    const form = new FormData()
-    form.append('is_submission', false)
-
-    return dispatch => {
-      dispatch(() => dispatch({
-        type: types.EVENTS.SAVE,
-        payload: DispatchAPI.events.save(token, id, form)
-          .then(json => {
-            if (queryAfter) {
-              dispatch(this.list(token, queryAfter))
-            }
-            return {
-              data: normalize(json, eventSchema)
-            }
-          })
-      }))
     }
   }
 
