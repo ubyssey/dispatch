a260dc7bfa81523690c34a0413103a85806cde0b
Jonathan Chapple <joncpl@gmail.com>
2017-06-20 15:59:49 -0700

    add tagsreducer

--- dispatch/static/manager/src/js/reducers/TagsReducer.js
+++ dispatch/static/manager/src/js/reducers/TagsReducer.js
@@ -3,12 +3,20 @@ import { combineReducers } from 'redux'
 import * as types from '../constants/ActionTypes'
 
 import {
+  Reducer,
+  fulfilled,
   buildManyResourceReducer,
   buildSingleResourceReducer
 } from '../util/redux'
 
 
+const newIdReducer = new Reducer(0)
+newIdReducer.handle(fulfilled(types.TAGS.CREATE), (state, action) => {
+  return action.payload.data.result
+})
+
 export default combineReducers({
   list: buildManyResourceReducer(types.TAGS).getReducer(),
   single: buildSingleResourceReducer(types.TAGS).getReducer(),
+  newId: newIdReducer.getReducer()
 })
