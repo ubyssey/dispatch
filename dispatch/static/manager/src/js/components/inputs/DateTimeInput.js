e85974c143735fdeb8fec13daa203af29a0dcb7e
Jonathan Chapple <joncpl@gmail.com>
2017-06-21 16:16:56 -0700

    datetimeinput improvements
    putting on top fixes an issue where openning it causes errant window behaviour as it changes the height of body
    
    also set max date higher than the default so events can be added to next year
    (default is always dec. 31 current year)

--- dispatch/static/manager/src/js/components/inputs/DateTimeInput.js
+++ dispatch/static/manager/src/js/components/inputs/DateTimeInput.js
@@ -4,6 +4,9 @@ import { DateTimePicker } from '@blueprintjs/datetime'
 
 import { humanizeDatetime } from '../../util/helpers'
 
+const TODAY = new Date()
+const MAX_DATE = new Date(TODAY.getFullYear()+1,12,31)
+
 export default function DateTimeInput(props) {
   const textString = props.value ?
     humanizeDatetime(props.value)
@@ -15,6 +18,10 @@ export default function DateTimeInput(props) {
       onChange={props.onChange}
       timePickerProps={{
         onChange: props.onChange
+      }}
+      datePickerProps={{
+        minDate: TODAY,
+        maxDate: MAX_DATE
       }} />
   )
 
@@ -23,7 +30,7 @@ export default function DateTimeInput(props) {
       <Popover
         content={picker}
         popoverClassName='pt-popover-content-sizing'
-        position={props.position || Position.BOTTOM}>
+        position={props.position || Position.TOP}>
         <div className='pt-control-group'>
           <button className='pt-button pt-icon-calendar c-input--datetime-button' />
           <input
