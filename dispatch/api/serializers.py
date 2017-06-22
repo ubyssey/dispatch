79695036e36bc32b34cda2acd38037b0254e3009
Jonathan Chapple <joncpl@gmail.com>
2017-06-22 11:33:51 -0700

    #598 patched in front end

--- dispatch/api/serializers.py
+++ dispatch/api/serializers.py
@@ -514,8 +514,6 @@ class ZoneSerializer(serializers.Serializer):
 
 class EventSerializer(DispatchModelSerializer):
 
-    image = serializers.ImageField(required=False, validators=[ValidFilename])
-
     class Meta:
         model = Event
         fields = (
