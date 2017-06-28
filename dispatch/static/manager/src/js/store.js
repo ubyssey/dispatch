21bdc2e256341d11f471afd750a231905a9be13c
Jonathan Chapple <joncpl@gmail.com>
2017-06-28 11:38:37 -0700

    reorganise store

--- dispatch/static/manager/src/js/store.js
+++ dispatch/static/manager/src/js/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { useRouterHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux'
@@ -6,13 +6,34 @@ import { createHistory } from 'history'
 import { loadingBarReducer, loadingBarMiddleware } from 'react-redux-loading-bar'
 import promiseMiddleware from 'redux-promise-middleware'
 
-import appReducer from '../reducers/AppReducer'
-import modalReducer from '../reducers/ModalReducer'
+import appReducer from './reducers/AppReducer'
+import modalReducer from './reducers/ModalReducer'
 
 const BASE_PATH = '/admin'
 
 const browserHistory = useRouterHistory(createHistory)({ basename: BASE_PATH })
 const router = routerMiddleware(browserHistory)
+
+let middleware = [
+  thunk,
+  router,
+  promiseMiddleware(),
+  loadingBarMiddleware({
+    promiseTypeSuffixes: ['PENDING', 'FULFILLED', 'REJECTED'],
+  })
+]
+
+let composeEnhancers = compose
+
+if (process.env.NODE_ENV == 'development') {
+  composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  const loggerOptions = {
+    collapsed: true,
+    level: 'info'
+  }
+  const logger = require('redux-logger').createLogger(loggerOptions)
+  middleware = [...middleware, logger]
+}
 
 export const store = createStore(
   combineReducers({
@@ -21,15 +42,7 @@ export const store = createStore(
     loadingBar: loadingBarReducer,
     modal: modalReducer
   }),
-  applyMiddleware(
-    thunk,
-    router,
-    promiseMiddleware(),
-    loadingBarMiddleware({
-      promiseTypeSuffixes: ['PENDING', 'FULFILLED', 'REJECTED'],
-    })
-  )
+  composeEnhancers(applyMiddleware(...middleware))
 )
-
 
 export const history = syncHistoryWithStore(browserHistory, store)
