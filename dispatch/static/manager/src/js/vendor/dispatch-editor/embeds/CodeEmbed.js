55d8e5cab70812543a0f36a9e42503ce063d68fd
Peter Siemens <peterjsiemens@gmail.com>
2017-06-27 23:15:07 -0400

    Fix imports, change to functional component

--- dispatch/static/manager/src/js/vendor/dispatch-editor/embeds/CodeEmbed.js
+++ dispatch/static/manager/src/js/vendor/dispatch-editor/embeds/CodeEmbed.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 import AceEditor from 'react-ace'
-import { Button } from '@blueprintjs/core'
-import SelectInput from '../../inputs/SelectInput'
+
+import SelectInput from '../../../components/inputs/SelectInput'
 
 import 'brace/mode/html'
 import 'brace/mode/css'
@@ -17,51 +17,37 @@ const MODES = [
   { value: 'javascript', label: 'javascript' }
 ]
 
-const DEFAULTVALUE = '<div>Enter html code here</div>'
-
-class CodeEmbedComponent extends React.Component {
-
-  onCodeChange(newValue) {
-    this.props.updateField('embedValue', newValue)
-  }
-
-  changeMode(mode) {
-    this.props.updateField('mode',mode)
-  }
-
-  render() {
-    return(
-      <div className='o-embed o-embed--code'>
-        <AceEditor
-          mode={this.props.data.mode}
-          theme='chrome'
-          width='100%'
-          height='300px'
-          value={this.props.data.embedValue}
-          defaultValue={this.props.data.embedValue ? this.props.data.embedValue : DEFAULTVALUE}
-          showPrintMargin={false}
-          onChange={(e) => this.onCodeChange(e)}
-          className='o-embed--code_editor'
-          editorProps={{$blockScrolling: true}}
-        />
-        <div className='o-embed--code_editor_button_container'>
-          <div className='o-embed--code_editor_mode_select'>
-            <SelectInput
-              options={MODES}
-              selected={this.props.data.mode}
-              onChange={(e) => this.changeMode(e.target.value)}/>
-          </div>
+function CodeEmbedComponent(props) {
+  return(
+    <div className='o-embed o-embed--code'>
+      <AceEditor
+        className='o-embed--code_editor'
+        mode={props.data.mode}
+        theme='chrome'
+        width='100%'
+        height='300px'
+        value={props.data.content}
+        showPrintMargin={false}
+        onChange={(content) => props.updateField('content', content)}
+        editorProps={{$blockScrolling: true}}
+      />
+      <div className='o-embed--code_editor_button_container'>
+        <div className='o-embed--code_editor_mode_select'>
+          <SelectInput
+            options={MODES}
+            selected={props.data.mode}
+            onChange={(e) => props.updateField('mode', e.target.value)}/>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default {
   type: 'code',
   component: CodeEmbedComponent,
   defaultData: {
-    embedValue: '',
+    content: '',
     mode: 'html',
   }
 }
