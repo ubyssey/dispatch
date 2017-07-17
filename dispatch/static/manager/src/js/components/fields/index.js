0c0923f8e12bc627e474b0089360748021bde526
Jonathan Chapple <joncpl@gmail.com>
2017-07-17 11:05:55 -0700

    missed in last commit

--- dispatch/static/manager/src/js/components/fields/index.js
+++ dispatch/static/manager/src/js/components/fields/index.js
@@ -3,11 +3,17 @@ import TextField from './TextField'
 import ArticleField from './ArticleField'
 import ImageField from './ImageField'
 import EventField from './EventField'
+import DateTimeField from './DateTimeField'
+import IntField from './IntField'
+import UIntField from './UIntField'
 
 export {
   CharField as char,
   TextField as text,
   ArticleField as article,
   ImageField as image,
-  EventField as event
+  EventField as event,
+  DateTimeField as datetime,
+  IntField as int,
+  UIntField as uint
 }
