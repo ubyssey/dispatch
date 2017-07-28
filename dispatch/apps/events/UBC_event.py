b11f58be2314d3483b027801a85451af39795360
Axel-Jacobsen <axelnj44@gmail.com>
2017-07-27 17:46:21 -0700

    UBC Event processing class

---
+++ dispatch/apps/events/UBC_event.py
@@ -0,0 +1,58 @@
+import requests, re
+from HTMLParser import HTMLParser
+
+class UBCEventError(Exception):
+    pass
+
+class TagStripper(HTMLParser):
+    """Class using the HTMLParser class to strip html tags from a string"""
+
+    def __init__(self):
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, data):
+        self.fed.append(data)
+
+    def get_data(self):
+        return ' '.join(self.fed)
+
+    def strip_tags(self, html):
+
+        self.feed(html)
+
+        return self.get_data()
+
+class UBCEvent(object):
+    """UBC Event class"""
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_html(self):
+        """Gets the html page from self.url and returns the relevent data"""
+
+        html = requests.get(self.url).text
+
+        m = re.search('.*<td class="fieldval description" colspan="[0-9]+">(.*)<\/td>.*', html)
+
+        if m:
+            return m.group(1)
+        else:
+            raise UBCEventError('URL Provided is not a valid UBC Event url')
+
+    def format_data(self):
+        """Returns formatted data from unformatted data"""
+
+        unformatted_data = self.get_html
+
+        formatting_data = unformatted_data.replace('<br />', '\n')
+        formatting_data = formatting_data.replace('&amp;', '&')
+        formatting_data = formatting_data.replace('<td class="fieldname">', '\n\n')
+
+        tag_stripper = TagStripper()
+        description = tag_stripper.strip_tags(formatting_data)
+
+        return {
+            'description': description
+        }
