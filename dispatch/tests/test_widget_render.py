5edfa5082c8b4f6d5d65ec591410a1b0d6737d2b
Jonathan Chapple <joncpl@gmail.com>
2017-08-02 13:30:15 -0700

    add tests for widget templatetag kwargs

--- dispatch/tests/test_widget_render.py
+++ dispatch/tests/test_widget_render.py
@@ -1,4 +1,5 @@
 from django.template import loader
+from django.utils.html import mark_safe
 
 from dispatch.theme import register
 from dispatch.theme.widgets import Zone, Widget
@@ -17,6 +18,8 @@ class TestWidget(Widget):
     id = 'test-widget'
     name = 'Test widget'
     template = 'widgets/test-widget.html'
+
+    valid_extra_ctx_kw = ('extra',)
 
     zones = [TestZone]
 
@@ -245,5 +248,63 @@ class WidgetRenderTestCase(DispatchAPITestCase, DispatchMediaTestMixin):
         result = template.render()
 
         html = u'\n\n<div class="zone">\n\n</div>\n'
+
+        self.assertEqual(result, html)
+
+
+    def test_zone_render_kwargs(self):
+        """The test zone should be properly rendered when provided valid kwargs"""
+
+        register.zone(TestZone)
+        register.widget(TestWidget)
+
+        zone = TestZone()
+        widget = TestWidget()
+
+        validated_data = {
+            'widget': 'test-widget',
+            'data': {
+              'title': 'test title 1',
+              'description': 'test description'
+            }
+        }
+
+        zone.save(validated_data)
+
+        template = loader.get_template('widgets/test-widget-kwargs.html')
+
+        result = template.render({ 'extra': mark_safe('<div class="testing">testing</div>') })
+
+        html = u'\n\n<div class="zone">\n<div class="widget">\n    <img class="title">test title 1</div>\n    <div class="description">test description</div>\n    \n    \n    <div class="testing">testing</div>\n</div>\n\n</div>\n'
+
+        self.assertEqual(result, html)
+
+    def test_zone_render_invalid_kwargs(self):
+        """The test zone/widget should ignore kwargs that aren't in the accepted list"""
+
+        register.zone(TestZone)
+        register.widget(TestWidget)
+
+        zone = TestZone()
+        widget = TestWidget()
+
+        validated_data = {
+            'widget': 'test-widget',
+            'data': {
+              'title': 'test title 1',
+              'description': 'test description'
+            }
+        }
+
+        zone.save(validated_data)
+
+        template = loader.get_template('widgets/test-widget-kwargs.html')
+
+        # in this case, data isn't in the list of accepted keywords,
+        # if it was, or the verification failed, then it would be overriden
+        # by the value provided in this context
+        result = template.render({ 'data': { 'title': 'some invalid data' }})
+
+        html = u'\n\n<div class="zone">\n<div class="widget">\n    <img class="title">test title 1</div>\n    <div class="description">test description</div>\n    \n    \n    \n</div>\n\n</div>\n'
 
         self.assertEqual(result, html)
