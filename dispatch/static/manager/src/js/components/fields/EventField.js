d78441ad44e3b1d559165d9ce8a71092000baf27
Jonathan Chapple <joncpl@gmail.com>
2017-08-03 13:13:50 -0700

    eventfield works in nested widget

--- dispatch/static/manager/src/js/components/fields/EventField.js
+++ dispatch/static/manager/src/js/components/fields/EventField.js
@@ -3,9 +3,16 @@ import React from 'react'
 import EventSelectInput from '../inputs/EventSelectInput'
 
 export default function EventField(props) {
+  const selected = props.data.map((event) => {
+    if (typeof event === 'object') {
+      return event.id
+    }
+    return event
+  })
+
   return (
     <EventSelectInput
-      selected={props.data}
+      selected={selected}
       many={props.many}
       onChange={selected => props.onChange(selected)} />
   )
