efd68841759ae1b7eeb224c5c70f1000d395f6c3
Peter Siemens <peterjsiemens@gmail.com>
2017-08-07 00:38:47 -0400

    Changes to event confirmation email

--- dispatch/apps/events/models.py
+++ dispatch/apps/events/models.py
@@ -1,24 +1,18 @@
-import urllib, os
-
-from django.core.files import File
+import urllib
+import os
+from phonenumber_field.modelfields import PhoneNumberField
 
 from django.db.models import (
-    Model, DateTimeField, CharField, TextField, PositiveIntegerField,
-    ImageField, FileField, BooleanField, ForeignKey,
-    SlugField, EmailField, SET_NULL)
-
+    Model, DateTimeField, CharField, TextField,
+    ImageField, BooleanField, EmailField)
+from django.core.files import File
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
 
-from dispatch.core.signals import post_create, post_update
-
-from phonenumber_field.modelfields import PhoneNumberField
-
 class Event(Model):
-
     title = CharField(max_length=255)
     description = TextField()
     host = CharField(max_length=255)
@@ -57,8 +51,7 @@ class Event(Model):
     submitter_phone = PhoneNumberField(null=True)
 
     def save_image_from_url(self, url):
-        """Store image locally if a Facebook URL is passed"""
-
+        """Store image locally if an external URL is passed"""
         result = urllib.urlretrieve(url)
 
         self.image.save(
@@ -68,32 +61,27 @@ class Event(Model):
 
         self.save()
 
-
-@receiver(post_update, sender=Event)
+@receiver(post_save, sender=Event)
 def send_approved_email(sender, instance, **kwargs):
-
-    print 'SENDING EMAIL IF is_submission IS TRUE:', instance.is_submission
-
-    if not instance.is_approved_email and instance.is_submission:
-
+    """Send an email to the submitter when the event is approved."""
+    if not instance.is_approved_email and not instance.is_submission:
         body = render_to_string('events/email/confirm.html', {'title': instance.title})
 
         send_mail(
-                'Your event has been approved!',
-                body,
-                settings.EMAIL_HOST_USER,
-                [instance.submitter_email],
-                fail_silently=True,
-            )
+            'Your event has been approved!',
+            body,
+            settings.EMAIL_HOST_USER,
+            [instance.submitter_email],
+            fail_silently=True,
+        )
 
         instance.is_approved_email = True
         instance.save()
 
-@receiver(post_update, sender=Event)
+@receiver(post_save, sender=Event)
 def send_published_email(sender, instance, **kwargs):
-
+    """Send an email to the submitter when the event has been published."""
     if instance.is_published and not instance.is_published_email:
-
         body = render_to_string('events/email/success.html', {'id': instance.id})
 
         send_mail(
