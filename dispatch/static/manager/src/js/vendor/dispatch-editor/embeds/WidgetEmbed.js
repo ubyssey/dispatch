682a8e431ee4fb067842974dc4b83de462c29c8e
Axel-Jacobsen <axelnj44@gmail.com>
2017-08-14 16:34:12 -0700

    Looping bug

--- dispatch/static/manager/src/js/vendor/dispatch-editor/embeds/WidgetEmbed.js
+++ dispatch/static/manager/src/js/vendor/dispatch-editor/embeds/WidgetEmbed.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import R from 'ramda'
 
 import WidgetSelectInput from '../../../components/inputs/WidgetSelectInput'
 import { FormInput } from '../../../components/inputs'
+import WidgetFields from '../../../components/WidgetFields'
+import Panel from '../../../components/Panel'
 
 function WidgetEmbedComponent(props) {
   console.log(props)
+  function updateWidgetField(fieldName, fieldValue) {
+    console.log('TESTA1')
+    props.updateField('data', R.assoc(fieldName, fieldValue, props.data.data))
+  }
   return (
-    <div className='o-embed o-embed--widget'>
-      <FormInput label='Widget'>
-        <WidgetSelectInput
-          zoneId='embed'
-          selected={props.data.widget_id}
-          update={widgetId => props.updateField('widget_id', widgetId)} />
-      </FormInput>
+    <div>
+      <div className='o-embed o-embed--widget'>
+        <Panel title='Widget'>
+          <FormInput>
+            <WidgetSelectInput
+              zoneId='embed'
+              selected={props.data.widget_id}
+              update={widgetId => props.updateField('widget_id', widgetId)} />
+          </FormInput>
+        </Panel>
+        <Panel title='Fields'>
+          <WidgetFields
+            updateField={updateWidgetField}
+            widgetId = {props.data.widget_id}
+            data = {props.data.data} />
+        </Panel>
+      </div>
     </div>
   )
 }
