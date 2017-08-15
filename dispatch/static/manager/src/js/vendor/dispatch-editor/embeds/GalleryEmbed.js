c68c4ce47e2761f5912f76bbb77bb144d2ff73ad
Jonathan Chapple <joncpl@gmail.com>
2017-08-15 16:34:28 -0700

    settimeout for loading galleryembed

--- dispatch/static/manager/src/js/vendor/dispatch-editor/embeds/GalleryEmbed.js
+++ dispatch/static/manager/src/js/vendor/dispatch-editor/embeds/GalleryEmbed.js
@@ -2,26 +2,36 @@ import React from 'react'
 
 import { FormInput, GallerySelectInput, TextInput } from '../../../components/inputs'
 
-function GalleryEmbedComponent(props) {
-  return (
-    <div className='o-embed o-embed--gallery'>
-      <form>
-        <FormInput label='Title'>
-          <TextInput
-            value={props.data.title}
-            placeholder='Title'
-            onChange={e => props.updateField('title', e.target.value)} />
-        </FormInput>
+class GalleryEmbedComponent extends React.Component {
+  constructor(props) {
+    super(props)
 
-        <FormInput label='Gallery'>
-          <GallerySelectInput
-            fill={true}
-            selected={props.data.id}
-            update={id => props.updateField('id', id)} />
-        </FormInput>
-      </form>
-    </div>
-  )
+    this.state = { show: false }
+    setTimeout(() => { this.setState({ show: true }) }, 1)
+  }
+
+  render() {
+    if (!this.state.show) { return null }
+    return (
+      <div className='o-embed o-embed--gallery'>
+        <form>
+          <FormInput label='Title'>
+            <TextInput
+              value={this.props.data.title}
+              placeholder='Title'
+              onChange={e => this.props.updateField('title', e.target.value)} />
+          </FormInput>
+
+          <FormInput label='Gallery'>
+            <GallerySelectInput
+              fill={true}
+              selected={this.props.data.id}
+              update={id => this.props.updateField('id', id)} />
+          </FormInput>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default {
