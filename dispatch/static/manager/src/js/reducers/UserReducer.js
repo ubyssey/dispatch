742c649a65e44f07358aba8dc3f58f87d9be48f7
Peter Siemens <peterjsiemens@gmail.com>
2017-08-21 22:53:26 -0700

    Merge develop

---
+++ dispatch/static/manager/src/js/reducers/UserReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+
+import * as types from '../constants/ActionTypes'
+
+import {
+  buildManyResourceReducer,
+  buildSingleResourceReducer
+} from '../util/redux'
+
+
+export default combineReducers({
+  list: buildManyResourceReducer(types.USERS).getReducer(),
+  single: buildSingleResourceReducer(types.USERS).getReducer(),
+})
