e5203978be11475cdeae7ef5229226f36c1bd46d
Peter Siemens <peterjsiemens@gmail.com>
2017-08-29 16:53:26 -0700

    Remove `get_revision` from PublishableManager

--- dispatch/modules/content/managers.py
+++ dispatch/modules/content/managers.py
@@ -3,13 +3,10 @@ import datetime
 from django.db.models import Manager
 
 class PublishableManager(Manager):
-
     def get(self, *args, **kwargs):
+        """Get the latest article with the given primary key."""
         if 'pk' in kwargs:
             kwargs['parent'] = kwargs['pk']
             kwargs['head'] = True
             del kwargs['pk']
-        return super(PublishableManager, self).get(*args, **kwargs)
-
-    def get_revision(self, *args, **kwargs):
         return super(PublishableManager, self).get(*args, **kwargs)
