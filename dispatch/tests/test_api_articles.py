c1baf7c9f26a77cddef0bff8add53b0f9458f499
Peter Siemens <peterjsiemens@gmail.com>
2017-08-29 18:00:46 -0700

    Add topic and tags article tests

--- dispatch/tests/test_api_articles.py
+++ dispatch/tests/test_api_articles.py
@@ -61,6 +61,125 @@ class ArticlesTests(DispatchAPITestCase):
         self.assertEqual(response.data['authors'][0]['full_name'], 'Test Person')
         self.assertEqual(response.data['slug'], 'test-article')
 
+    def test_update_article(self):
+        """Update the basic fields of an article"""
+
+        article = DispatchTestHelpers.create_article(self.client)
+
+        NEW_HEADLINE = 'New Headline'
+        NEW_SLUG = 'new-slug'
+        NEW_SNIPPET = 'New snippet'
+        NEW_IMPORTANCE = 5
+        NEW_SEO_KEYWORD = 'new keyword'
+        NEW_SEO_DESCRIPTION = 'new seo description'
+
+        data = {
+            'headline': NEW_HEADLINE,
+            'slug': NEW_SLUG,
+            'snippet': NEW_SNIPPET,
+            'importance': NEW_IMPORTANCE,
+            'seo_keyword': NEW_SEO_KEYWORD,
+            'seo_description': NEW_SEO_DESCRIPTION
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['headline'], NEW_HEADLINE)
+        self.assertEqual(response.data['slug'], NEW_SLUG)
+        self.assertEqual(response.data['snippet'], NEW_SNIPPET)
+        self.assertEqual(response.data['importance'], NEW_IMPORTANCE)
+        self.assertEqual(response.data['seo_keyword'], NEW_SEO_KEYWORD)
+        self.assertEqual(response.data['seo_description'], NEW_SEO_DESCRIPTION)
+
+        self.assertNotEqual(article.data['headline'], NEW_HEADLINE)
+        self.assertNotEqual(article.data['slug'], NEW_SLUG)
+        self.assertNotEqual(article.data['snippet'], NEW_SNIPPET)
+        self.assertNotEqual(article.data['importance'], NEW_IMPORTANCE)
+        self.assertNotEqual(article.data['seo_keyword'], NEW_SEO_KEYWORD)
+        self.assertNotEqual(article.data['seo_description'], NEW_SEO_DESCRIPTION)
+
+    def test_update_article_tags(self):
+        """Should be able to update and remove article tags"""
+
+        article = DispatchTestHelpers.create_article(self.client)
+
+        tag_1 = DispatchTestHelpers.create_tag(self.client, 'Tag 1')
+        tag_2 = DispatchTestHelpers.create_tag(self.client, 'Tag 2')
+        tag_3 = DispatchTestHelpers.create_tag(self.client, 'Tag 3')
+
+        data = {
+            'tag_ids': [tag_1.data['id'], tag_2.data['id']]
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['tags']), 2)
+        self.assertEqual(response.data['tags'][0]['id'], tag_1.data['id'])
+        self.assertEqual(response.data['tags'][1]['id'], tag_2.data['id'])
+
+        data = {
+            'tag_ids': [tag_1.data['id'], tag_3.data['id']]
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(len(response.data['tags']), 2)
+        self.assertEqual(response.data['tags'][0]['id'], tag_1.data['id'])
+        self.assertEqual(response.data['tags'][1]['id'], tag_3.data['id'])
+
+        data = {
+            'tag_ids': []
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['tags'], [])
+
+    def test_update_article_topic(self):
+        """Should be able to update and remove the article topic"""
+
+        article = DispatchTestHelpers.create_article(self.client)
+
+        topic_1 = DispatchTestHelpers.create_topic(self.client, 'Topic 1')
+        topic_2 = DispatchTestHelpers.create_topic(self.client, 'Topic 2')
+
+        data = {
+            'topic_id': topic_1.data['id']
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['topic']['id'], topic_1.data['id'])
+
+        data = {
+            'topic_id': topic_2.data['id']
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['topic']['id'], topic_2.data['id'])
+
+        data = {
+            'topic_id': None
+        }
+
+        url = reverse('api-articles-detail', args=[article.data['id']])
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['topic'], None)
+
     def test_delete_article_unauthorized(self):
         """Delete article should fail with unauthenticated request"""
 
