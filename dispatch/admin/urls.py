837b7f27af6bf4506dae5f01df9c76e5a3881919
Peter Siemens <peterjsiemens@gmail.com>
2017-09-27 15:20:43 -0700

    Extract CSS into separate stylesheet bundle

--- dispatch/admin/urls.py
+++ dispatch/admin/urls.py
@@ -8,7 +8,8 @@ def admin(request):
     """Render HTML entry point for manager app."""
     context = {
         'api_url': settings.API_URL,
-        'app_bundle': 'js/index-%s.js' % dispatch.__version__
+        'app_js_bundle': 'manager-%s.js' % dispatch.__version__,
+        'app_css_bundle': 'manager-%s.css' % dispatch.__version__
     }
     return render_to_response('manager/index.html', context)
 
--- dispatch/static/manager/webpack.config.js
+++ dispatch/static/manager/webpack.config.js
@@ -1,63 +1,54 @@
 var webpack = require('webpack');
 var version = require('./package.json').version;
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
- entry: {
-   index:  './src/js/index.js',
- },
- output: {
-   path: __dirname + '/dist/js',
-   filename: '[name]-' + version + '.js'
- },
- module: {
-   rules: [
-     {
-       test: /\.js$/,
-       exclude: /node_modules/,
-       use: { loader: 'babel-loader' },
-     },
-     {
-       test: /\.js$/,
+  entry: {
+    index:  './src/js/index.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'manager-' + version + '.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
-       enforce: 'pre',
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
         use: {
-            loader: 'eslint-loader' ,
-            options: {
-                emitWarning: true,
-            }
+          loader: 'eslint-loader',
+          options: {
+            emitWarning: true,
+          }
         }
-     },
-     {
-       test: /\.scss$/,
-       exclude: /node_modules/,
-       use: [
-         { loader: 'style-loader' },
-         {
-           loader: 'css-loader',
-           options: {
-             sourceMap: true
-           }
-         },
-         {
-           loader: 'sass-loader',
-           options: {
-             sourceMap: true
-           }
-         }
-       ]
-     },
-     {
-       test: /\.(png|jpg|woff|eot|ttf)$/,
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader',],
+        }),
+      },
+      {
+        test: /\.(png|jpg|woff|eot|ttf)$/,
         use: { loader: 'url-loader?limit=100000' }
-     }
-   ]
- },
- plugins: [
-   new webpack.DefinePlugin({
-     'process.env': {
-       'NODE_ENV': JSON.stringify('production')
-     }
-   }),
-   new webpack.optimize.UglifyJsPlugin()
- ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('manager-' + version + '.css')
+  ]
 };
