b066f00f909357f36a64007d03b4006ec747006c
Peter Siemens <peterjsiemens@gmail.com>
2017-12-06 18:46:00 -0800

    Move migration file

--- dispatch/migrations/0006_author_type.py
+++ dispatch/migrations/0006_author_type.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dispatch', '0001_initial'),
+        ('dispatch', '0005_remove_preview_id_null'),
     ]
 
     operations = [
