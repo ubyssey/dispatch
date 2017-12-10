bbe4db19d50bb25e8e9e664a1d06b0cacaddbb95
Ron Y <31330116+i-ron-y@users.noreply.github.com>
2017-12-10 02:19:23 -0800

    Implement VideoEditor

---
+++ dispatch/static/manager/src/js/components/VideoEditor/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import videosActions from '../../actions/VideosActions'
+import VideoForm from './VideoForm'
+
+import ItemEditor from '../ItemEditor'
+
+const TYPE = 'Video'
+const AFTER_DELETE = 'videos'
+
+const mapStateToProps = (state) => {
+	return {
+		listItem: state.app.videos.single,
+		entities: {
+			remote: state.app.entities.videos,
+			local: state.app.entities.local.videos,
+		},
+		token: state.app.auth.token
+	}
+}
+
+const mapDispatchToProps = (state) => {
+	return {
+		getListItem: (token, videoId) => {
+			dispatch(videosActions.get(token, videoId))
+		},
+		setListItem: (video) => {
+			dispatch(videosActions.set(video))
+		},
+		saveListItem: (token, videoId, data) => {
+			dispatch(videosActions.save(token, videoId, data))
+		},
+		createListItem: (token, data) => {
+			dispatch(videosActions.create(token, data, AFTER_DELETE))
+		},
+		deleteListItem: (token, videoId, next) => {
+			dispatch(videosActions.delete(token, videoId, next))
+		}
+	}
+}
+
+function VideoEditorComponent(props) {
+	return (
+		<ItemEditor
+		  type={TYPE}
+		  afterDelete={AFTER_DELETE}
+		  form={VideoForm}
+		  {... props} />
+	)
+}
+
+const VideoEditor = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(VideoEditorComponent)
+
+export default VideoEditor
