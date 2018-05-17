d31015aed6bf6acb67f997dee2be5c3acf86916a
Rowan Baker-French <rowanskye@gmail.com>
2018-05-17 11:09:45 -0700

    Update index.js

--- dispatch/static/manager/src/js/pages/Images/index.js
+++ dispatch/static/manager/src/js/pages/Images/index.js
@@ -1,9 +1,7 @@
 import ImagesIndexPage from './ImagesIndexPage'
 import EditImagePage from './EditImagePage'
-// import NewPersonPage from './NewPersonPage'
 
 export {
   EditImagePage as EditImage,
   ImagesIndexPage as Index,
-  // NewPersonPage as NewPerson
 }
