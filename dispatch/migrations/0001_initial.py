e520a75cad4091d9691c26a3a8e63569bd484457
Peter Siemens <peterjsiemens@gmail.com>
2018-06-20 12:16:17 -0500

    Add section slug column

--- dispatch/migrations/0001_initial.py
+++ dispatch/migrations/0001_initial.py
@@ -182,6 +182,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
+                ('slug', models.SlugField(unique=True)),
             ],
         ),
         migrations.CreateModel(
