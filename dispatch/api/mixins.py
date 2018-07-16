f3146e7c5c983b3459b8cea9836ad2457062d3d4
rowansdabomb <rowanskye@gmail.com>
2018-07-16 11:14:11 -0700

    remove mixin

--- dispatch/api/mixins.py
+++ dispatch/api/mixins.py
@@ -93,35 +93,6 @@ class DispatchPublishableMixin(object):
 
         return Response(serializer.data)
 
-    def pushNotification(self, article):
-        # grab each endpoint from list in database and make a push
-        data={
-            'headline': article.headline,
-            'url': article.get_absolute_url(),
-            'snippet': article.snippet,
-            'image': article.featured_image.image.get_thumbnail_url(),
-            'tag': 'ubyssey-standard-notification'
-        }
-        subscriptions = Subscription.objects.all()
-        for sub in subscriptions:
-            try:
-                webpush(
-                    subscription_info={
-                        "endpoint": sub.endpoint,
-                        "keys": {
-                            "p256dh": sub.p256dh,
-                            "auth": sub.auth
-                        }},
-                    data=json.dumps(data),
-                    vapid_private_key="Mp2OSApC5ZQ11iHtKfTfAWycrr-YYl9yphpkeqKIy9E",
-                    vapid_claims={
-                            "sub": "mailto:YourNameHere@example.org===",
-                        }
-                )
-            except WebPushException as ex:
-                sub.delete()
-
-
 class DispatchModelSerializer(HyperlinkedModelSerializer):
     def __init__(self, *args, **kwargs):
         # Override default constructor to call hide_authenticated_fields
