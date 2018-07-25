be2d0f1ce877e39d1cb3160dd31fb9a05cf57fda
Peter Siemens <peterjsiemens@gmail.com>
2018-07-24 17:04:45 -0700

    Remove extra line in forms.py

--- dispatch/admin/forms.py
+++ dispatch/admin/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from dispatch.models import User
 
-
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
