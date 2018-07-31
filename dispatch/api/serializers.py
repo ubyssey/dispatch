00ca418a8bb2712ded3a12621a6bab19bc4d0c3d
Jamie Lee <lee.jamr@gmail.com>
2018-07-31 10:14:38 -0700

    Revert "EXIF/XMP image metadata reading"

--- dispatch/api/serializers.py
+++ dispatch/api/serializers.py
@@ -213,7 +213,6 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
     filename = serializers.CharField(source='get_filename', read_only=True)
 
     title = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    caption = serializers.CharField(required=False, allow_null=True, allow_blank=True)
 
     url = serializers.CharField(source='get_absolute_url', read_only=True)
     url_medium = serializers.CharField(source='get_medium_url', read_only=True)
@@ -228,7 +227,7 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     tag_ids = serializers.ListField(
         write_only=True,
-        required=True,
+        required=False,
         child=serializers.IntegerField())
 
     width = serializers.IntegerField(read_only=True)
@@ -241,7 +240,6 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
             'img',
             'filename',
             'title',
-            'caption',
             'authors',
             'author_ids',
             'tags',
@@ -259,21 +257,16 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
         return self.update(Image(), validated_data)
 
     def update(self, instance, validated_data):
-        is_new = instance.pk is None
-
         instance = super(ImageSerializer, self).update(instance, validated_data)
 
         # Save authors
-        author_ids = validated_data.get('author_ids')
-        if author_ids:
-            instance.save_authors(author_ids)
+        authors = validated_data.get('author_ids')
+        if authors:
+            instance.save_authors(authors)
 
-        # Save_tags
-        tag_ids = validated_data.get('tag_ids')
-        if tag_ids:
-            # Do not clear tags for first save. This prevents deletion of EXIF tags
-            clear = not is_new
-            instance.save_tags(tag_ids, clear=clear)
+        tag_ids = validated_data.get('tag_ids', False)
+        if tag_ids != False:
+            instance.save_tags(tag_ids)
 
         return instance
 
@@ -643,9 +636,9 @@ class ArticleSerializer(DispatchModelSerializer, DispatchPublishableSerializer):
         if featured_video != False:
             instance.save_featured_video(featured_video)
 
-        author_ids = validated_data.get('author_ids')
-        if author_ids:
-            instance.save_authors(author_ids, is_publishable=True)
+        authors = validated_data.get('author_ids')
+        if authors:
+            instance.save_authors(authors, is_publishable=True)
 
         tag_ids = validated_data.get('tag_ids', False)
         if tag_ids != False:
