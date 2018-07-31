3f925a8456038e12acd9ddd18cba560b67c7275e
JamieRL <lee.jamr@gmail.com>
2018-07-31 14:02:07 -0700

    fix migrations

--- dispatch/migrations/0016_breaking_news.py
+++ dispatch/migrations/0016_breaking_news.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dispatch', '0012_polls'),
+        ('dispatch', '0015_image_wrap'),
     ]
 
     operations = [
