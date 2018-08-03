a7bb77448875889412935e498e6510da9fb67284
JamieRL <lee.jamr@gmail.com>
2018-08-03 13:32:07 -0700

    remove commented code

--- dispatch/api/mixins.py
+++ dispatch/api/mixins.py
@@ -38,9 +38,6 @@ class DispatchPublishableMixin(object):
 
             if version is not None:
                 queryset = queryset.filter(revision_id=version)
-            # elif self.request.method == 'DELETE':
-            #     if queryset:
-            #         queryset = queryset.filter(id=F('parent_id'))
             else:
                 queryset = queryset.filter(head=True)
         else:
