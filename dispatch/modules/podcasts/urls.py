47f935d273e92be2463253395fe5701c94fafc76
JamieRL <lee.jamr@gmail.com>
2018-08-21 15:06:09 -0700

    move podcast feeds files into podcasts module

--- dispatch/modules/podcasts/urls.py
+++ dispatch/modules/podcasts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from dispatch.rss.feeds import PodcastFeed
+from dispatch.moduels.podcasts.feeds import PodcastFeed
 
 urlpatterns = [
     url(r'^podcasts/(?P<slug>[-\w]+)/', PodcastFeed(), name='rss'),
