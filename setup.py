e9d8bf18f6b27dc7042875b5ee348c720f678742
JamieRL <lee.jamr@gmail.com>
2018-08-27 11:18:19 -0700

    Version 0.5.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='0.5.8',
+    version='0.5.9',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
