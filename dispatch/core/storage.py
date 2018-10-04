5153df7ed747947e43ca159f44fb435b4433c856
rowansdabomb <rowanskye@gmail.com>
2018-10-04 12:29:09 -0700

    add LOCAL_DEV variables to settings files

--- dispatch/core/storage.py
+++ dispatch/core/storage.py
@@ -9,17 +9,16 @@ def generate_signed_url(path, content_type, expires_after_seconds=60):
 
     obj = os.path.join(settings.GS_LOCATION, path)
 
-    # ######### FOR LOCAL DEV PURPOSES ONLY ##########
-    # dirname = os.path.dirname
-    # ######### make gcs-local.json the name of your service-account key file
-    # key_file = os.path.join('/', 'ubyssey.ca', 'ubyssey.ca', 'gcs-local.json') 
-    # os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=key_file
-    # client = storage.Client().from_service_account_json(key_file)
-    # ################################################
-
-    ######### FOR PRODUCTION PURPOSES ONLY ##########
-    client = storage.Client()
-    ###################################################
+    if settings.LOCAL_DEV:
+        dirname = os.path.dirname
+        ##############################################################################
+        # Ensure you have a valid service-account json key file and set its path below
+        ##############################################################################
+        key_file = os.path.join('/', 'ubyssey.ca', 'ubyssey.ca', 'gcs-local.json') 
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=key_file
+        client = storage.Client().from_service_account_json(key_file)
+    else:
+        client = storage.Client()
     
     bucket = client.get_bucket(settings.GS_STORAGE_BUCKET_NAME)
     blob = storage.Blob(obj, bucket)
