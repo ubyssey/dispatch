a8d8901a5dcb2309b7229e29f3ecf25a9813a181
Peter Siemens <peterjsiemens@gmail.com>
2018-10-09 21:54:36 -0700

    Version 0.5.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='0.5.14',
+    version='0.5.15',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
