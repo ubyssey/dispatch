e2b03e1de7db7a8d50095fe8a78fea5ecd95d5b4
Rowansdabomb <rowanskye@gmail.com>
2018-10-24 17:47:05 -0700

    update dispatch to version 0.5.23

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='0.5.22',
+    version='0.5.23',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
