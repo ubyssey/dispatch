0cd7b6d0d5cc0f32c99bf0e404c33e10abfe17dd
Rowansdabomb <rowanskye@gmail.com>
2018-10-24 17:44:16 -0700

    fix missing line in storage.py

--- dispatch/core/storage.py
+++ dispatch/core/storage.py
@@ -9,10 +9,10 @@ def generate_signed_url(path, content_type, expires_after_seconds=60):
 
     obj = os.path.join(settings.GS_LOCATION, path)
 
-    if settings.DEBUG:
-        client = storage.Client().from_service_account_json(settings.GCS_CREDENTIALS_FILE)
-    else:
-        client = storage.Client()
+    client = storage.Client()
+
+    # if settings.DEBUG:
+    #   client = storage.Client().from_service_account_json(settings.GCS_CREDENTIALS_FILE)
     
     bucket = client.get_bucket(settings.GS_STORAGE_BUCKET_NAME)
     blob = storage.Blob(obj, bucket)
