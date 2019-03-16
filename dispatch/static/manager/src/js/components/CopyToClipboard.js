446bb19b4fd52061f98fc4d4e2aacb466a2cd11e
Razvan Nesiu <razvannesiu@gmail.com>
2019-03-15 17:06:03 -0700

    Eliminate unnecessary classNames

--- dispatch/static/manager/src/js/components/CopyToClipboard.js
+++ dispatch/static/manager/src/js/components/CopyToClipboard.js
@@ -3,12 +3,12 @@ import React from 'react'
 class CopyToClipboard extends React.Component{
   render(){
     return (
-      <div className="cols">
+      <div>
         <textarea
           ref={(node) => { this.txtInput=node }}
           value={this.props.text} />
         <button 
-          className="center bp3-button bp3-icon-large bp3-icon-clipboard" 
+          className="bp3-button bp3-icon-large bp3-icon-clipboard" 
           onClick={() => { this.txtInput.select(); document.execCommand('copy')}} />
       </div>
     )
