d87dfc3d331eade3eeebd3ba95cd8df2170912b5
Razvan Nesiu <razvannesiu@gmail.com>
2019-05-17 15:29:46 -0700

    Add another rb in open call

--- dispatch/tests/test_api_images.py
+++ dispatch/tests/test_api_images.py
@@ -178,7 +178,7 @@ class ImagesTests(DispatchAPITestCase, DispatchMediaTestMixin):
 
         url = reverse('api-images-list')
 
-        with open(self.get_input_file('test_image_a.jpg')) as test_image:
+        with open(self.get_input_file('test_image_a.jpg'), 'rb') as test_image:
             image = self.client.post(url, { 'img': test_image }, format='multipart')
 
         person = Person.objects.create(full_name='Test Person')
