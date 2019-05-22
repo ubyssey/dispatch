502355ba01228b124c2a08464bc84ad5ed8ae448
Razvan Nesiu <razvannesiu@gmail.com>
2019-05-22 11:46:47 -0700

    Update dispatch to version 0.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='0.5.31',
+    version='0.6.0',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
