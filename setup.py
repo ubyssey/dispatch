a5c1299f1079dfd00949ef5e2159475bd4af6cbe
Razvan Nesiu <razvannesiu@gmail.com>
2019-06-20 12:59:00 -0700

    Update dispatch to v0.7.0 (running with django v2.2)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='0.6.5',
+    version='0.7.0',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
