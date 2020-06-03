6e115cd80bfdf18b6c3ae57e1924e06f24969efc
Keegan Landrigan <keeganland@gmail.com>
2020-06-03 16:38:47 -0700

    patching version vars

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='1.0.0',
+    version='1.0.1',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
