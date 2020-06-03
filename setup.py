ddd51d269636a5e95d475afc2b44a3cd14fab74d
Keegan Landrigan <keeganland@gmail.com>
2020-06-03 15:57:05 -0700

    update version number variables

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='0.8.1',
+    version='0.9.0',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
