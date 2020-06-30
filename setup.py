c76921d0a18d26889ed2de98242dcacd82ed2eb9
davidkao0902 <davidkao0902@hotmail.com>
2020-06-30 22:00:46 +0000

    version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='1.2.1',
+    version='1.3.0',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
