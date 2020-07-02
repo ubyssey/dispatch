df19c037a58cabab1d5e97307bfc7fd40e849db2
Keegan Landrigan <keeganland@gmail.com>
2020-07-02 23:18:24 +0000

    revise dispatch_filters.py logic to use regex

--- dispatch/templatetags/dispatch_filters.py
+++ dispatch/templatetags/dispatch_filters.py
@@ -6,18 +6,19 @@ import re
 @register.filter(name='youtube_embed_id')
 # converts youtube URL into embed HTML
 def youtube_embed_id(url):
-    urlParts = url.split("?v=")
-    if len(urlParts) > 1:
-        # embed_url = 'https://www.youtube.com/embed/%s' %(match.group(2))
-        return urlParts[1]
+    youtube_regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
+    match = youtube_regex.match(url)
+    if match:
+        return match.group('id')        
     return ''
 
 @register.filter(name='youtube_embed_url')
 # converts youtube URL into embed HTML
 def youtube_embed_url(url):
-    urlParts = url.split("?v=")
-    if len(urlParts) > 1:
-        embed_url = 'https://www.youtube.com/embed/%s' %(urlParts[1])
+    youtube_regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
+    match = youtube_regex.match(url)
+    if match:
+        embed_url = 'https://www.youtube.com/embed/%s' %(match.group('id'))
         
         return embed_url
     return ''
