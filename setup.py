4fc450eba886b4f2bdafda1f296807875866b9d1
Keegan Landrigan <keeganland@gmail.com>
2020-07-02 17:04:34 -0700

    increment missed version variable in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dispatch',
-    version='1.3.1',
+    version='1.3.2',
     description='A publishing platform for modern newspapers',
     url='https://github.com/ubyssey/dispatch',
     author='Peter Siemens',
