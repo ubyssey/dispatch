386641331c86abb432d3193af77cd53180c5b727
Keegan Landrigan <keeganland@gmail.com>
2020-08-28 01:48:24 +0000

    remove high coupling request processing logic

--- dispatch/modules/content/managers.py
+++ dispatch/modules/content/managers.py
@@ -10,22 +10,4 @@ class PublishableManager(Manager):
             kwargs['head'] = True
             del kwargs['pk']
 
-        """If the url requested includes the querystring parameters 'version' and 'preview_id',
-        get the article with the specified version and preview_id.
-
-        Otherwise, get the published version of the article.
-        """
-
-        if 'request' in kwargs:
-	       	request = kwargs['request']
-	       	version = request.GET.get('version', None)
-	       	preview_id = request.GET.get('preview_id', None)
-
-	       	if (version is not None) and (preview_id is not None):
-	       		kwargs['revision_id'] = version
-	       		kwargs['preview_id'] = preview_id
-	       		del kwargs['is_published']
-
-	       	del kwargs['request']
-
         return super(PublishableManager, self).get(*args, **kwargs)
