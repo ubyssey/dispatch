2588c3bce774a988e9d2bbbcf0b341d3ed7d1dbc
Keegan Landrigan <keeganland@gmail.com>
2021-06-14 10:58:40 -0700

    Update setup.py
    bump django version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='dispatch',
     scripts=['dispatch/bin/dispatch-admin'],
     include_package_data=True,
     install_requires=[
-        'django == 3.1.8',
+        'django == 3.1.12',
         'djangorestframework == 3.12.2',
         'pillow',
         'requests',
